d6e0c285c66b82dfe75d6f3af6ab615550971687
Glenn <glenn.latomme@gmail.com>
2021-01-08 11:24:01 +0100

    chore: Removing tests with no goal
    We only test components that have real functionallity, so most component tests are useless

--- code/frontend/packages/ranking-client/src/app/_shared/pipes/level-to-letter/level-to-letter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LevelToLetterPipe } from './level-to-letter.pipe';
-
-describe('LevelToLetterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new LevelToLetterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
