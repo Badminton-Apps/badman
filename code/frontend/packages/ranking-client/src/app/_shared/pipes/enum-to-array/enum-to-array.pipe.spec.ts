54b6320edcb4377f30598dfde1ebdd6f53f16d24
Glenn <glenn.latomme@gmail.com>
2021-01-08 11:24:01 +0100

    chore: Removing tests with no goal
    We only test components that have real functionallity, so most component tests are useless

--- code/frontend/packages/ranking-client/src/app/_shared/pipes/enum-to-array/enum-to-array.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EnumToArrayPipe } from './enum-to-array.pipe';
-
-describe('EnumToArrayPipe', () => {
-  it('create an instance', () => {
-    const pipe = new EnumToArrayPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
