09877358493b2e631b3815204fd672e46e80f8b0
Glenn <glenn.latomme@gmail.com>
2021-01-24 15:50:42 +0100

    feat: linking players to club
    Gaps betweens years are only registered when importing chronological

--- code/backend/packages/_shared/models/sequelize/team-membership.model.ts
+++ code/backend/packages/_shared/models/sequelize/team-membership.model.ts
@@ -5,13 +5,18 @@ import {
   Table,
   Model
 } from 'sequelize-typescript';
+import { BuildOptions } from 'sequelize/types';
 import { Player } from './player.model';
 import { Team } from './team.model';
 
 @Table({
-  schema: "public"
+  schema: 'public'
 })
 export class TeamMembership extends Model<TeamMembership> {
+  constructor(values?: Partial<TeamMembership>, options?: BuildOptions) {
+    super(values, options);
+  }
+
   @ForeignKey(() => Player)
   @Column({ unique: 'unique_constraint' })
   playerId: number;
@@ -20,6 +25,7 @@ export class TeamMembership extends Model<TeamMembership> {
   @Column({ unique: 'unique_constraint' })
   teamId: number;
 
+  @Column({ unique: 'unique_constraint' })
   start: Date;
 
   @Column
