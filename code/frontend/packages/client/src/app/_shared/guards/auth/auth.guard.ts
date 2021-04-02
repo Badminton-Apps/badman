01cae74ac1e9b61e79aef1349bfa09d5050cb020
Glenn <glenn.latomme@gmail.com>
2021-04-02 21:47:05 +0200

    chore: Merge pt2

--- code/frontend/packages/client/src/app/_shared/guards/auth/auth.guard.ts
+++ code/frontend/packages/client/src/app/_shared/guards/auth/auth.guard.ts
@@ -1,19 +1,22 @@
-import { AuthService } from '../../services/auth/auth.service';
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import {
   ActivatedRouteSnapshot,
+  CanActivate,
+  Params,
   RouterStateSnapshot,
   UrlTree,
-  CanActivate
 } from '@angular/router';
-import { Observable, merge, combineLatest } from 'rxjs';
-import { tap, combineAll, map } from 'rxjs/operators';
+import { environment } from 'environments/environment';
+import { combineLatest, Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { AuthService } from '../../services/security/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private snackBar: MatSnackBar) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
@@ -24,7 +27,9 @@ export class AuthGuard implements CanActivate {
     if (next.data.claims) {
       if (typeof next.data.claims === 'string') {
         canActivateObservables$.push(
-          this.auth.hasClaim$(next.data.claims as string)
+          this.auth.hasClaim$(
+            this.replaceParams(next.params, [next.data.claims])[0]
+          )
         );
       } else {
         if (next.data.claims.any) {
@@ -32,21 +37,26 @@ export class AuthGuard implements CanActivate {
             next.data.claims.any = [next.data.claims.any];
           }
           canActivateObservables$.push(
-            this.auth.hasAnylaims$(next.data.claims.any)
+            this.auth.hasAnyClaims$(
+              this.replaceParams(next.params, next.data.claims.any)
+            )
           );
         } else if (next.data.claims.all) {
           if (typeof next.data.claims.all === 'string') {
             next.data.claims.all = [next.data.claims.all];
           }
           canActivateObservables$.push(
-            this.auth.hasAllClaims$(next.data.claims.all)
+            this.auth.hasAllClaims$(
+              this.replaceParams(next.params, next.data.claims.all)
+            )
           );
         }
       }
     } else {
+      // not allowed
       canActivateObservables$.push(
         this.auth.isAuthenticated$.pipe(
-          tap(loggedIn => {
+          tap((loggedIn) => {
             if (!loggedIn) {
               this.auth.login(state.url);
             }
@@ -58,7 +68,25 @@ export class AuthGuard implements CanActivate {
     return combineLatest(canActivateObservables$).pipe(
       map((results: boolean[]) => {
         return results.reduce((acc, value) => acc && value, true);
+      }),
+      tap((r) => {
+        if (r == false) {
+          if (environment.production == false) {
+            console.warn('No permissions', next.data.claims);
+          }
+
+          this.snackBar.open("You don't have the permissions for this");
+        }
       })
     );
+  }
+
+  private replaceParams(params: any, claims: string[]): string[] {
+    // replace with params
+    for (const [key, value] of Object.entries(params)) {
+      claims = claims.map(c => c.replace(`[:${key}]`, value as string));
+    }
+
+    return claims;
   }
 }
--- code/frontend/packages/client/src/app/competition/competition.module.ts
+++ code/frontend/packages/client/src/app/competition/competition.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { SharedModule } from 'app/_shared';
 import { CompetitionRoutingModule } from './competition-routing.module';
+import { DetailCompetitionComponent } from './pages';
+
+const materialModules = [
+  MatListModule,
+  MatMenuModule,
+  MatButtonModule,
+  MatIconModule,
+];
 
 @NgModule({
-  declarations: [],
-  imports: [CompetitionRoutingModule],
+  declarations: [DetailCompetitionComponent],
+  imports: [SharedModule, ...materialModules, CompetitionRoutingModule],
 })
 export class CompetitionModule {}
