0f2b73a01c4c251f2d06f55af0ec5c5a16b0dbb0
Glenn <glenn.latomme@gmail.com>
2021-04-02 21:47:05 +0200

    chore: Merge pt2

--- code/frontend/packages/client/src/app/_shared/models/team.model.ts
+++ code/frontend/packages/client/src/app/_shared/models/team.model.ts
@@ -1,15 +1,88 @@
+import { SubEvent } from './events/sub-event.model';
 import { Player } from './player.model';
 
 export class Team {
   id: string;
   name: string;
   abbreviation: string;
-  players: Player[];
+  type: string;
+  number: number;
+  active: boolean;
+  preferredTime: string;
+  preferredDay:
+    | 'sunday'
+    | 'monday'
+    | 'tuesday'
+    | 'wednesday'
+    | 'thursday'
+    | 'friday'
+    | 'saturday';
 
-  constructor(args: Partial<Team>) {
-    this.id = args.id;
-    this.name = args.name;
-    this.abbreviation = args.abbreviation;
-    this.players = args.players?.map((p) => new Player(p));
+  players: Player[];
+  subEvents: SubEvent[];
+
+  baseIndex: number;
+  captain: Player;
+  captainId: string;
+
+  constructor(args?: Partial<Team>) {
+    this.id = args?.id;
+    this.name = args?.name;
+    this.number = args?.number;
+    this.abbreviation = args?.abbreviation;
+    this.type = args?.type;
+    this.active = args?.active;
+
+    this.preferredTime = args?.preferredTime;
+    this.preferredDay = args?.preferredDay;
+    this.subEvents = args?.subEvents?.map((s) => new SubEvent(s));
+    this.captain = args.captain ? new Player(args.captain) : null;
+    this.captainId = args.captain?.id;
+
+    this.players =
+      args?.players?.map((p) => {
+        let index = this.type == 'MX' ? 36 : 24;
+
+        if (p.rankingPlaces && p.rankingPlaces.length > 0) {
+          if (this.type == 'MX') {
+            index =
+              p.rankingPlaces[0].single +
+              p.rankingPlaces[0].double +
+              p.rankingPlaces[0].mix;
+          } else {
+            index = p.rankingPlaces[0].single + p.rankingPlaces[0].double;
+          }
+        }
+
+        return new Player({ ...p, index });
+      }) ?? [];
+
+    this.calculateBase();
+  }
+
+  private calculateBase() {
+    const basePlayers = this.players.filter((r) => r.base);
+    const missingPlayers = basePlayers.length >= 4 ? 0 : 4 - basePlayers.length;
+
+    if (this.type == 'MX') {
+      this.baseIndex = basePlayers.reduce((acc, cur) => {
+        const rankingPlace = cur.rankingPlaces[0] ?? {
+          single: 12,
+          double: 12,
+          mix: 12,
+        };
+        return (
+          acc + rankingPlace.single + rankingPlace.double + rankingPlace.mix
+        );
+      }, missingPlayers * 36);
+    } else {
+      this.baseIndex = basePlayers.reduce((acc, cur) => {
+        const rankingPlace = cur.rankingPlaces[0] ?? {
+          single: 12,
+          double: 12,
+        };
+        return acc + rankingPlace.single + rankingPlace.double;
+      }, missingPlayers * 24);
+    }
   }
 }
