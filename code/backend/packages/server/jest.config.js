62ee0645159f6a14b2ba6fa67737b9acbacdf482
Glenn <glenn.latomme@gmail.com>
2021-04-11 08:20:46 +0200

    fix: don't know if this makes any difference

--- code/backend/packages/server/jest.config.js
+++ code/backend/packages/server/jest.config.js
@@ -8,22 +8,7 @@ module.exports = {
   collectCoverageFrom: ['**/*.{ts,tsx}', '!**/node_modules/**', '!**/vendor/**'],
   collectCoverage: true,
   setupFiles: ['<rootDir>/../../test/env.js'],
-  reporters: [
-    'default',
-    [
-      'jest-junit',
-      {
-        outputDirectory: 'coverage',
-        uniqueOutputName: false,
-        suiteName: 'Test results',
-        classNameTemplate: '{classname}-{title}',
-        titleTemplate: '{classname}-{title}',
-        ancestorSeparator: ' › ',
-        usePathForSuiteName: true,
-        includeConsoleOutput: true
-      }
-    ]
-  ],
+  reporters: ['default'],
   testEnvironment: 'node',
   coverageReporters: ['text-summary', 'lcov']
 };
