22d38479ccf13b203738efc2a2a9fb4fded93c3f
Glenn <glenn.latomme@gmail.com>
2021-04-11 10:54:37 +0200

    fix: reject on empty file
    also renamed to normal ts files

---
+++ code/backend/packages/import/src/import/__tests__/file.spec.ts
@@ -0,0 +1,64 @@
+import { csvToArray } from '@badvlasim/shared';
+import { join } from 'path';
+import { CompetitionCpProcessor } from '../processors';
+
+describe('Wrong file', () => {
+  let service: CompetitionCpProcessor;
+  let fileLocation: string;
+
+  beforeAll(async () => {
+    fileLocation = join(process.cwd(), 'src/import/__tests__/files/competition_wrong_file.cp');
+
+    service = new CompetitionCpProcessor();
+  });
+
+  it('Should throw error on import wrong competition', async () => {
+    // Arrange
+    expect.assertions(1);
+
+    // Act
+    try {
+      await service.importFile(fileLocation);
+    } catch (e) {
+      // Assert
+      expect(e?.message).toEqual("Couldn't find file.");
+    }
+  });
+});
+
+describe('Empty file', () => {
+  let service: CompetitionCpProcessor;
+  let fileLocation: string;
+
+  beforeAll(async () => {
+    fileLocation = join(process.cwd(), 'src/import/__tests__/files/empty.cp');
+
+    service = new CompetitionCpProcessor();
+  });
+
+  it('Should throw error on import empty file', async () => {
+    // Arrange
+    expect.assertions(1);
+
+    // Act
+    try {
+      await service.importFile(fileLocation);
+    } catch (e) {
+      // Assert
+      expect(e?.message).toEqual('no data');
+    }
+  });
+
+  test('should trhow error on empty csv', async () => {
+    // Arrange
+    expect.assertions(1);
+
+    // Act
+    try {
+      await csvToArray('');
+    } catch (e) {
+      // Assert
+      expect(e?.message).toMatch('No data');
+    }
+  });
+});
