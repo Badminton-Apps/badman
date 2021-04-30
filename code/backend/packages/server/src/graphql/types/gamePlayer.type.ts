2ed3727e839fccf27c4b9fea11ef1322759f2427
Glenn <glenn.latomme@gmail.com>
2021-04-30 09:47:56 +0200

    fix: linting

--- code/backend/packages/server/src/graphql/types/gamePlayer.type.ts
+++ code/backend/packages/server/src/graphql/types/gamePlayer.type.ts
@@ -32,7 +32,7 @@ const GamePlayerType = new GraphQLObjectType({
             findOptions.where = {
               ...findOptions.where,
               rankingDate: { [Op.lte]: game.playedAt }
-              //rankingDate: '2019-12-13 00:00:00+01'
+              // rankingDate: '2019-12-13 00:00:00+01'
             };
             findOptions.order = [['rankingDate', 'DESC']];
             findOptions.limit = 1;
