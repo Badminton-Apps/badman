a3e5d83540b545d06bea59a4eee1a3687224c72a
Glenn <glenn.latomme@gmail.com>
2021-07-18 11:49:29 +0200

    feat: #130 - Store team information

---
+++ code/backend/packages/_shared/database/migrations/20210718074948-adding metadata field.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, sequelize) => {
+    return queryInterface.sequelize.transaction(async t => {
+      try {
+        await queryInterface.addColumn(
+          {
+            tableName: 'TeamSubEventMemberships',
+            schema: 'event'
+          },
+          'meta',
+          {
+            type: sequelize.DataTypes.TEXT,
+            allowNull: true
+          },
+          { transaction: t }
+        );
+      } catch (err) {
+        console.error(err);
+        t.rollback();
+      }
+    });
+  },
+
+  down: async (queryInterface, sequelize) => {
+    return queryInterface.sequelize.transaction(async t => {
+      await queryInterface.removeColumn(
+        {
+          tableName: 'TeamSubEventMemberships',
+          schema: 'event'
+        },
+        'meta',
+        { transaction: t }
+      );
+    });
+  }
+};
