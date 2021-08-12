db04cce88fa5c5ffd5e53b7f4e25522dffce01e3
Glenn <glenn.latomme@gmail.com>
2021-08-12 09:19:59 +0200

    fix: Must be logged-in, if not redirect to login and back
    - Also if no permissions: show message and redirect to home page

--- code/frontend/packages/client/src/app/competition/modules/change-encounter/change-encounter-routing.module.ts
+++ code/frontend/packages/client/src/app/competition/modules/change-encounter/change-encounter-routing.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from 'app/_shared';
 
 import { ChangeEncounterComponent } from './pages/change-encounter/change-encounter.component';
 
-const routes: Routes = [{ path: '', component: ChangeEncounterComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: ChangeEncounterComponent,
+    canActivate: [AuthGuard],
+    data: {
+      claims: {
+        any: ['*_change:encounter', 'change-any:encounter'],
+      },
+    },
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ChangeEncounterRoutingModule { }
+export class ChangeEncounterRoutingModule {}
