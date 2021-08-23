7dafa4ff912330ecdcb067595b11e4ddf3a66a55
Glenn <glenn.latomme@gmail.com>
2021-08-23 11:45:20 +0200

    fixup! 0d3ea168aafcff026790840244e4862f8f308cfa

--- code/backend/packages/import/src/import/__tests__/competition_cp.spec.ts
+++ code/backend/packages/import/src/import/__tests__/competition_cp.spec.ts
@@ -73,7 +73,6 @@ describe('competition cp', () => {
     expect(importerFiles.length).toBe(1);
     const importerFile = importerFiles[0];
     expect(importerFile.name).toEqual('Victor League 2019-2020');
-    expect(importerFile.uniCode).toEqual('201903290920109453');
     expect(importerFile.firstDay.toISOString()).toEqual('2019-09-27T22:00:00.000Z');
   });
 
@@ -145,7 +144,6 @@ describe('competition cp 2', () => {
     expect(importerFiles.length).toBe(1);
     const importerFile = importerFiles[0];
     expect(importerFile.name).toEqual('PBA competitie 2021-2022');
-    expect(importerFile.uniCode).toEqual('202103220037589421');
     expect(importerFile.firstDay.toISOString()).toEqual('2021-08-31T22:00:00.000Z');
   });
 
@@ -153,7 +151,6 @@ describe('competition cp 2', () => {
     // Arrange
     const importFile = await new ImporterFile({
       name: 'PBA competitie 2021-2022',
-      uniCode: '202103220037589421',
       fileLocation,
       firstDay: new Date(),
       type: EventImportType.COMPETITION_CP
