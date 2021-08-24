f7053e3e64bacab3b2dc4a53f8feaf792a1a62f1
Glenn <glenn.latomme@gmail.com>
2021-08-24 11:16:39 +0200

    fix: Adding locations

--- code/backend/scripts/basic/src/merge-accounts.ts
+++ code/backend/scripts/basic/src/merge-accounts.ts
@@ -7,8 +7,13 @@ import { DataBaseHandler, Player } from '../../../packages/_shared';
 
 async function merge_accounts() {
   const databaseService = new DataBaseHandler(dbConfig.default);
-  const destination = `75dc3dbe-14bb-482c-8fc9-c7de72518d47`;
-  const source = `d794e1ac-6f7f-4c4a-b292-f371c4d90c69`;
+  const destination = `59e8539b-2a5c-4ffd-a60b-947349fba543`;
+  const sources =[
+    `00ae8d83-fce3-4db1-8374-dacdd02f4ad0`,
+    `8b1faa9f-22bc-4c57-9b2b-db782ef8c315`
+  ];
 
-  await databaseService.mergePlayers(destination, source);
+  for (const source of sources) {
+    await databaseService.mergePlayers(destination, source);
+  }
 }
