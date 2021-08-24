ad73ae0957c5ac8f25416399c7e3aa90af33faab
Glenn <glenn.latomme@gmail.com>
2021-08-24 11:41:31 +0200

    feat: merge mailing to single function with checks

--- code/backend/scripts/basic/src/merge-accounts.ts
+++ code/backend/scripts/basic/src/merge-accounts.ts
@@ -7,13 +7,11 @@ import { DataBaseHandler, Player } from '../../../packages/_shared';
 
 async function merge_accounts() {
   const databaseService = new DataBaseHandler(dbConfig.default);
-  const destination = `59e8539b-2a5c-4ffd-a60b-947349fba543`;
-  const sources =[
-    `00ae8d83-fce3-4db1-8374-dacdd02f4ad0`,
-    `8b1faa9f-22bc-4c57-9b2b-db782ef8c315`
-  ];
+  const destination = `2517a521-74dc-4305-baa7-a394dc46558d`;
+  const sources = [`4ca337b5-06fe-4653-adc0-49e78448d4ee`];
 
   for (const source of sources) {
     await databaseService.mergePlayers(destination, source);
   }
 }
+
