e1e442d5baaae25afd3923e61e4426031e892246
Glenn <glenn.latomme@gmail.com>
2021-08-31 20:23:02 +0200

    feat: pdf export part 1

---
+++ code/backend/packages/server/src/controllers/index.ts
@@ -0,0 +1,9 @@
+// start:ng42.barrel
+export * from './enrollement.controller';
+export * from './pdf.controller';
+export * from './ranking.controller';
+export * from './request-link.controller';
+export * from './system.controller';
+export * from './user.controller';
+// end:ng42.barrel
+
