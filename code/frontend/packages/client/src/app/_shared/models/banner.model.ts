14784d31ea12f1d807d1b530956fa1448c3a21f1
Glenn Latomme <glenn.latomme@gmail.com>
2021-09-14 10:26:58 +0200

    feat: adds (sorry)

---
+++ code/frontend/packages/client/src/app/_shared/models/banner.model.ts
@@ -0,0 +1,12 @@
+export class Banner {
+    constructor(
+        public adClient: string,
+        public adSlot: number,
+        public adFormat: string,
+        public fullWidthResponsive: boolean) {
+        this.adClient = adClient;
+        this.adSlot = adSlot;
+        this.adFormat = adFormat || 'auto';
+        this.fullWidthResponsive = fullWidthResponsive || true;
+    }
+}
