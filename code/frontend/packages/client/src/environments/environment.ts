bc0af1273cffde3b8e68beaa71db56c6c38b4436
Glenn Latomme <glenn.latomme@gmail.com>
2021-09-15 10:36:40 +0200

    fixup! 36614cd

--- code/frontend/packages/client/src/environments/environment.ts
+++ code/frontend/packages/client/src/environments/environment.ts
@@ -1,6 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import { version } from '../../package.json';
 
 export const environment = {
   production: false,
@@ -10,7 +11,8 @@ export const environment = {
   adsense: {
     adClient: 'ca-pub-2426855871474715',
     show: true
-  }
+  },
+  version
 };
 
 /*
