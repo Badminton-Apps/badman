0bcbea01f3bb545c76be04d4b109b6942f344be3
Glenn Latomme <glenn.latomme@gmail.com>
2021-09-15 10:21:30 +0200

    feat: cookies

--- code/frontend/packages/client/src/app/info/info-routing.module.ts
+++ code/frontend/packages/client/src/app/info/info-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ChangelogComponent } from './pages/changelog/changelog.component';
+import { CookiesComponent } from './pages/cookies/cookies.component';
 import { LandingComponent } from './pages/landing/landing.component';
 
-const routes: Routes = [{ path: '', component: LandingComponent }, { path: 'changelog', component: ChangelogComponent }];
+const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'changelog', component: ChangelogComponent },
+  { path: 'cookies', component: CookiesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class InfoRoutingModule { }
+export class InfoRoutingModule {}
