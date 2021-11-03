8bae5439a88fe480b68fa6b84b94e2a11f8d0ae1
Glenn <glenn.latomme@gmail.com>
2021-11-03 19:40:19 +0100

    feat: start of event detail pages

---
+++ code/frontend/packages/client/src/app/competition/pages/detail-sub-event-competition/detail-sub-event-competition.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './detail-sub-event-competition.component.html',
+  styleUrls: ['./detail-sub-event-competition.component.scss']
+})
+export class DetailSubEventCompetitionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
