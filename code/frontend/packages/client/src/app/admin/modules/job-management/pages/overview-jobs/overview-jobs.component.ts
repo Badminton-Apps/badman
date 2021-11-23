7c539f6633371681c09df3ed77b38e8d923c18ef
Glenn <glenn.latomme@gmail.com>
2021-11-23 16:41:40 +0100

    fix: Double encounters

---
+++ code/frontend/packages/client/src/app/admin/modules/job-management/pages/overview-jobs/overview-jobs.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Apollo } from 'apollo-angular';
+import { Subscription } from 'rxjs';
+import * as cronQuery from './graphql/getCronStatusQuery.graphql';
+
+@Component({
+  templateUrl: './overview-jobs.component.html',
+  styleUrls: ['./overview-jobs.component.scss'],
+})
+export class OverviewJobsComponent implements OnInit, OnDestroy {
+  dataSource = new MatTableDataSource();
+  @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
+  @ViewChild(MatSort, { static: false }) sort!: MatSort;
+
+  resultsLength = 0;
+  isLoadingResults = false;
+  isRateLimitReached = false;
+
+  displayedColumns: string[] = ['running', 'name', 'scheduled', 'cron', 'done', 'options'];
+
+  private querySubscription!: Subscription;
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit(): void {
+    this.querySubscription = this.apollo
+      .watchQuery<any>({
+        query: cronQuery,
+        pollInterval: 5000,
+      })
+      .valueChanges.subscribe(({ data, loading }) => {
+        this.isLoadingResults = loading;
+        this.dataSource.data = data.crons;
+      });
+  }
+
+  ngOnDestroy() {
+    this.querySubscription.unsubscribe();
+  }
+}
