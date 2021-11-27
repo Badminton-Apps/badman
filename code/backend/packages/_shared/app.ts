bbbabbc17f30a4e1776bd6fc2f6b9bf78d2836d3
Glenn <glenn.latomme@gmail.com>
2021-11-27 12:40:20 +0100

    feat: upgraded backend packages

--- code/backend/packages/_shared/app.ts
+++ code/backend/packages/_shared/app.ts
@@ -1,11 +1,11 @@
-import { start } from 'elastic-apm-node';
-export const apm = start({
+require('elastic-apm-node').start({
   serviceName: process.env.SERVICE_NAME,
   serverUrl: process.env.APM_SERVER_URL,
   secretToken: process.env.APM_SERVER_TOKEN,
   verifyServerCert: false,
   disableSend: process.env.NODE_ENV !== 'production'
 }); 
+
 
 import { logger } from '@badvlasim/shared';
 import cors from 'cors';
@@ -44,7 +44,8 @@ export class App {
       'http://localhost:5000',
       'http://localhost:4000',
       'http://localhost:4200',
-      'https://badman.app'
+      'https://badman.app',
+      'https://studio.apollographql.com'
     ];
     this.corsOptions = {
       origin: (origin, callback) => {
