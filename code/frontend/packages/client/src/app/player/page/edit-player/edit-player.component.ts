d3902757b49497306fb229c7f12491871e79c568
Glenn <glenn.latomme@gmail.com>
2021-11-28 15:21:39 +0100

    feat: add/edit player ranking

--- code/frontend/packages/client/src/app/player/page/edit-player/edit-player.component.ts
+++ code/frontend/packages/client/src/app/player/page/edit-player/edit-player.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Claim, Player, PlayerService, RankingPlace, SystemService } from 'app/_shared';
+import { Claim, Player, PlayerService, RankingPlace, RankingSystem, SystemService } from 'app/_shared';
 import { ClaimService } from 'app/_shared/services/security/claim.service';
-import { combineLatest, Observable, lastValueFrom } from 'rxjs';
+import { combineLatest, Observable, lastValueFrom, tap } from 'rxjs';
 import { filter, map, switchMap } from 'rxjs/operators';
 @Component({
   templateUrl: './edit-player.component.html',
@@ -10,6 +10,7 @@ import { filter, map, switchMap } from 'rxjs/operators';
 })
 export class EditPlayerComponent implements OnInit {
   player$!: Observable<Player>;
+  system?: RankingSystem;
 
   constructor(
     private playerService: PlayerService,
@@ -21,7 +22,10 @@ export class EditPlayerComponent implements OnInit {
   ngOnInit(): void {
     const id$ = this.route.paramMap.pipe(map((x) => x.get('id')));
 
-    const system$ = this.systemService.getPrimarySystem().pipe(filter((x) => !!x));
+    const system$ = this.systemService.getPrimarySystem().pipe(
+      filter((x) => !!x),
+      tap((x) => (this.system = x ?? undefined))
+    );
 
     this.player$ = combineLatest([id$, system$]).pipe(
       switchMap(([playerId, system]) => this.playerService.getPlayer(playerId!, system!.id!))
@@ -32,8 +36,8 @@ export class EditPlayerComponent implements OnInit {
     await lastValueFrom(this.claimService.updateGlobalUserClaim(player.id!, args.claim.id!, args.checked));
   }
 
-  async onRankingPlaceChanged(rankingPlace: Partial<RankingPlace>) {
-    await lastValueFrom(this.playerService.updatePlayerRanking(rankingPlace));
+  async onRankingPlaceChanged(rankingPlace: Partial<RankingPlace>, player: Player) {
+    await lastValueFrom(this.playerService.updatePlayerRanking(rankingPlace, player.id!));
   }
   async onPlayerUpdated(player: Partial<Player>) {
     await lastValueFrom(this.playerService.updatePlayer(player));
