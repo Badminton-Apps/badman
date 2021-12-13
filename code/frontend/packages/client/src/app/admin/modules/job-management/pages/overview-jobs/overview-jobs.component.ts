2aafcbd57016422fc566334c98108926dc3f7e0a
Glenn <glenn.latomme@gmail.com>
2021-12-13 19:13:01 +0100

    feat: Showing cron explanation for job list

--- code/frontend/packages/client/src/app/admin/modules/job-management/pages/overview-jobs/overview-jobs.component.ts
+++ code/frontend/packages/client/src/app/admin/modules/job-management/pages/overview-jobs/overview-jobs.component.ts
@@ -5,6 +5,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { Apollo } from 'apollo-angular';
 import { Subscription } from 'rxjs';
 import * as cronQuery from './graphql/getCronStatusQuery.graphql';
+import cronstrue from 'cronstrue';
 
 @Component({
   templateUrl: './overview-jobs.component.html',
@@ -33,7 +34,9 @@ export class OverviewJobsComponent implements OnInit, OnDestroy {
       })
       .valueChanges.subscribe(({ data, loading }) => {
         this.isLoadingResults = loading;
-        this.dataSource.data = data.crons;
+        this.dataSource.data = data.crons?.map((cron: any) => {
+          return { ...cron, tooltip: cronstrue.toString(cron.cron) };
+        });
       });
   }
 
