abba581b45efbf57158e42c50ae1cf8392d2310c
Glenn <glenn.latomme@gmail.com>
2021-12-18 15:45:56 +0100

    feat: level breakdown

---
+++ code/backend/packages/server/src/graphql/queries/points.query.ts
@@ -0,0 +1,19 @@
+import { RankingPoint } from '@badvlasim/shared/models';
+import { GraphQLList } from 'graphql';
+import { defaultListArgs, resolver } from 'graphql-sequelize';
+import { queryFixer } from '../queryFixer';
+import { RankingPointType } from '../types';
+
+export const pointsQuery = {
+  type: new GraphQLList(RankingPointType),
+  args: Object.assign(defaultListArgs(), {}),
+  resolve: resolver(RankingPoint, {
+    before: async (findOptions: { [key: string]: object }) => {
+      findOptions = {
+        ...findOptions,
+        where: queryFixer(findOptions.where)
+      };
+      return findOptions;
+    }
+  })
+};
