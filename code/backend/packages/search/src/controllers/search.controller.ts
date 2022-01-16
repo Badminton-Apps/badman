a1efc3a0587d23b68b1e5f473af0595110b7127b
Glenn <glenn.latomme@gmail.com>
2022-01-16 12:27:47 +0100

    Tests for search controller

--- code/backend/packages/search/src/controllers/search.controller.ts
+++ code/backend/packages/search/src/controllers/search.controller.ts
@@ -1,4 +1,4 @@
-import { AuthenticatedRequest, BaseController, Player } from '@badvlasim/shared';
+import { AuthenticatedRequest, BaseController, logger, Player } from '@badvlasim/shared';
 import { RequestHandler, Response, Router } from 'express';
 import { Op } from 'sequelize';
 
@@ -17,29 +17,43 @@ export class SearchController extends BaseController {
 
   private _search = async (request: AuthenticatedRequest, response: Response) => {
     // Right now we querying the database. but we will use elasticsearch in the future.
-    const parts =
-      `${request?.query?.query}`
-        ?.toLowerCase()
-        ?.replace(/[;\\\\/:*?\"<>|&',]/, ' ')
-        ?.split(' ') ?? [];
-    const queries = [];
-    for (const part of parts) {
-      queries.push({
-        [Op.or]: [
-          { firstName: { [Op.iLike]: `%${part}%` } },
-          { lastName: { [Op.iLike]: `%${part}%` } },
-          { memberId: { [Op.iLike]: `%${part}%` } }
-        ]
+    try {
+      const parts =
+        `${request?.query?.query}`
+          ?.toLowerCase()
+          ?.replace(/[;\\\\/:*?\"<>|&',]/, ' ')
+          ?.split(' ')
+          ?.map((r) => r.trim())
+          ?.filter((r) => r?.length > 0)
+          ?.filter((r) => (r ?? null) != null) ?? [];
+
+      const queries = [];
+      for (const part of parts) {
+        console.log(`Add part ${part}`);
+        queries.push({
+          [Op.or]: [
+            { firstName: { [Op.iLike]: `%${part}%` } },
+            { lastName: { [Op.iLike]: `%${part}%` } },
+            { memberId: { [Op.iLike]: `%${part}%` } }
+          ]
+        });
+      }
+
+      if (queries.length === 0) {
+        return response.status(400).send({ message: 'No query provided' });
+      }
+
+      // Temporary structure to return the results.
+      const result = await Player.findAll({
+        attributes: ['slug', 'id', 'memberId', 'firstName', 'lastName', 'gender'],
+        where: { [Op.and]: queries },
+        limit: 100
       });
+
+      response.status(200).send(result);
+    } catch (error) {
+      logger.error(`Something went wrong:`, { error });
+      response.status(500).send({ message: error.message || 'Something went wrong' });
     }
-
-    // Temporary structure to return the results.
-    const result = await Player.findAll({
-      attributes: ['slug', 'id', 'memberId', 'firstName', 'lastName', 'gender'],
-      where: { [Op.and]: queries },
-      limit: 100
-    });
-
-    response.status(200).send(result);
   };
 }
