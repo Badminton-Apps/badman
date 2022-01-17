22f8f5660d22c88a2586b4147f715308cd1449ad
Glenn <glenn.latomme@gmail.com>
2022-01-17 14:22:38 +0100

    fix: Removing unwanted console.log

--- code/frontend/packages/client/src/app/modules/game/components/game-info/game-info.component.ts
+++ code/frontend/packages/client/src/app/modules/game/components/game-info/game-info.component.ts
@@ -23,7 +23,5 @@ export class GameInfoComponent implements OnInit {
 
     this.playerT2P1 = this.game.players?.find(p => p.team === 2 && p.player === 1);
     this.playerT2P2 = this.game.players?.find(p => p.team === 2 && p.player === 2);
-
-    console.log(this.game.round, this.game.order);
   }
 }
