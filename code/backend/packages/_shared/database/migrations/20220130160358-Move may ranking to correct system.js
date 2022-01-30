c55daf4bb4a10d9cfd6f50d22ec79b651cf119b1
Glenn <glenn.latomme@gmail.com>
2022-01-30 17:19:38 +0100

    feat: Use primary system for assembly

---
+++ code/backend/packages/_shared/database/migrations/20220130160358-Move may ranking to correct system.js
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, sequelize) => {
+    return queryInterface.sequelize.transaction(async (t) => {
+      try {
+        const [systems] = await queryInterface.sequelize.query(
+          `select * from ranking."Systems" `,
+          { transaction: t }
+        );
+
+        const primSystem = systems.find((s) => s.name == 'BBF Rating').id;
+        const current = systems.find((s) => s.name == 'BV 75/30 FINAL SYSTEM').id;
+
+        await queryInterface.sequelize.query(
+          `UPDATE ranking."Places" set "SystemId" = '${primSystem}' where "SystemId" = '${current}' and "rankingDate" = '2021-05-15 00:00:00+02'`,
+          { transaction: t }
+        );
+      } catch (err) {
+        console.error('We errored with', err);
+        t.rollback();
+      }
+    });
+  },
+
+  down: async (queryInterface) => {
+    return queryInterface.sequelize.transaction(async (t) => {
+      try {
+        const [systems] = await queryInterface.sequelize.query(
+          `select * from ranking."Systems" `,
+          { transaction: t }
+        );
+
+        const primSystem = systems.find((s) => s.name == 'BBF Rating').id;
+        const current = systems.find((s) => s.name == 'BV 75/30 FINAL SYSTEM').id;
+
+        await queryInterface.sequelize.query(
+          `UPDATE ranking."Places" set "SystemId" = '${current}' where "SystemId" = '${primSystem}' and "rankingDate" = '2021-05-15 00:00:00+02'`,
+          { transaction: t }
+        );
+      } catch (err) {
+        console.error('We errored with', err);
+        t.rollback();
+      }
+    });
+  },
+};
