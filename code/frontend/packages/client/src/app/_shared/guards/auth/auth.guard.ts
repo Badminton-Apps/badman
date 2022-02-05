d14a644d778be0ef2f4c966191d70cb131ce5634
Glenn <glenn.latomme@gmail.com>
2022-02-05 12:31:28 +0100

    feat: login and loading

--- code/frontend/packages/client/src/app/_shared/guards/auth/auth.guard.ts
+++ code/frontend/packages/client/src/app/_shared/guards/auth/auth.guard.ts
@@ -5,33 +5,43 @@ import { AuthService } from '@auth0/auth0-angular';
 import { TranslateService } from '@ngx-translate/core';
 import { ClaimService } from 'app/_shared';
 import { environment } from 'environments/environment';
-import { combineLatest, Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { combineLatest, Observable, of, Subject } from 'rxjs';
+import { debounceTime, map, switchMap, tap, finalize } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
+  private loader$ = new Subject<boolean>();
+  public loader = false;
+
   constructor(
     private auth: AuthService,
     private claimService: ClaimService,
     private snackBar: MatSnackBar,
     private router: Router,
     private translate: TranslateService
-  ) {}
+  ) {
+    this.loader$.pipe(debounceTime(300)).subscribe((loader) => {
+      this.loader = loader;
+    });
+  }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean | UrlTree> | boolean {
+    this.loader$.next(true);
     const canActivateObservables$: Observable<boolean>[] = [];
 
     // If we have AuthGuard you have to be logged in
     canActivateObservables$.push(
       this.auth.isAuthenticated$.pipe(
-        tap((loggedIn) => {
+        switchMap((loggedIn) => {
           if (!loggedIn) {
-            this.auth.loginWithRedirect();
+            return this.auth.loginWithPopup().pipe(map(() => true));
+          } else {
+            return of(true);
           }
         })
       )
@@ -39,18 +49,24 @@ export class AuthGuard implements CanActivate {
 
     if (next?.data?.['claims'] && next.data?.['claims'].length > 0) {
       if (typeof next.data?.['claims'] === 'string') {
-        canActivateObservables$.push(this.claimService.hasClaim$(this.replaceParams(next.params, [next.data?.['claims']])[0]));
+        canActivateObservables$.push(
+          this.claimService.hasClaim$(this.replaceParams(next.params, [next.data?.['claims']])[0])
+        );
       } else {
         if (next.data?.['claims'].any) {
           if (typeof next.data?.['claims'].any === 'string') {
             next.data['claims'].any = [next.data?.['claims'].any];
           }
-          canActivateObservables$.push(this.claimService.hasAnyClaims$(this.replaceParams(next.params, next.data?.['claims'].any)));
+          canActivateObservables$.push(
+            this.claimService.hasAnyClaims$(this.replaceParams(next.params, next.data?.['claims'].any))
+          );
         } else if (next.data?.['claims'].all) {
           if (typeof next.data?.['claims'].all === 'string') {
             next.data['claims'].all = [next.data?.['claims'].all];
           }
-          canActivateObservables$.push(this.claimService.hasAllClaims$(this.replaceParams(next.params, next.data?.['claims'].all)));
+          canActivateObservables$.push(
+            this.claimService.hasAllClaims$(this.replaceParams(next.params, next.data?.['claims'].all))
+          );
         }
       }
     }
@@ -64,9 +80,12 @@ export class AuthGuard implements CanActivate {
           if (environment.production == false) {
             console.warn('No permissions', next.data?.['claims']);
           }
-          this.snackBar.open(this.translate.instant("permission.no-perm"));
+          this.snackBar.open(this.translate.instant('permission.no-perm'));
           this.router.navigate(['/']);
         }
+      }),
+      finalize(() => {
+        this.loader$.next(false);
       })
     );
   }
