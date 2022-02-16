069e01c21b039cc8a8760488f36e4f8e506dd755
Glenn <glenn.latomme@gmail.com>
2022-02-16 13:30:47 +0100

    feat: show new place when evaluating

--- code/frontend/packages/client/src/app/_shared/models/player.model.ts
+++ code/frontend/packages/client/src/app/_shared/models/player.model.ts
@@ -2,6 +2,7 @@ import { Club } from 'app/_shared';
 import { Game } from './game.model';
 import { RankingPlace } from './ranking-place.model';
 import * as moment from 'moment';
+import { RankingSystem } from '.';
 
 export class Player {
   id?: string;
@@ -18,11 +19,13 @@ export class Player {
   base?: boolean;
   isClaimed = false;
   rankingPlaces?: RankingPlace[];
-  lastRanking?: RankingPlace;
+  lastRankingPlaces?: RankingPlace[];
   games?: Game[];
   index?: number;
   competitionPlayer?: boolean;
   updatedAt?: Date;
+  rankingSystem?: RankingSystem;
+  lastRanking?: RankingPlace;
 
   clubs?: Club[];
   club?: Club;
@@ -39,6 +42,7 @@ export class Player {
     this._fullName = args?.fullName;
     this.isClaimed = args?.isClaimed ?? false;
     this.lastRanking = (args?.lastRanking ?? null) != null ? new RankingPlace(args?.lastRanking) : undefined;
+    this.rankingSystem = (args?.rankingSystem ?? null) != null ? new RankingSystem(args?.rankingSystem) : undefined;
     this.games = args?.games?.map((g) => new Game(g));
     this.base = args?.base;
     this.sub = args?.sub;
@@ -50,6 +54,7 @@ export class Player {
     this.updatedAt = args?.updatedAt != null ? new Date(args.updatedAt) : undefined;
 
     this.rankingPlaces = args?.rankingPlaces?.map((r) => new RankingPlace(r));
+    this.lastRankingPlaces = args?.lastRankingPlaces?.map((r) => new RankingPlace(r));
     if ((this.lastRanking ?? null) == null && this.rankingPlaces != null && this.rankingPlaces.length > 0) {
       this.lastRanking = this.rankingPlaces?.sort((a, b) => a.rankingDate!.getTime() - b.rankingDate!.getTime())[0];
     }
