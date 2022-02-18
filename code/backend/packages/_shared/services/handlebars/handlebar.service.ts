d75723fb7e71c487b498bf3f7b20d98091b728bf
Glenn <glenn.latomme@gmail.com>
2022-02-18 11:47:53 +0100

    fix: Simulate with empty player could cause error

--- code/backend/packages/_shared/services/handlebars/handlebar.service.ts
+++ code/backend/packages/_shared/services/handlebars/handlebar.service.ts
@@ -251,37 +251,37 @@ export class HandlebarService {
         preppedMap,
         based,
         teamed,
-        input.team.double[0][0],
-        input.team.double[0][1]
+        input.team.double?.[0]?.[0],
+        input.team.double?.[0]?.[1]
       ),
       this._addPlayer(
         preppedMap,
         based,
         teamed,
-        input.team.double[1][0],
-        input.team.double[1][1]
+        input.team.double?.[1]?.[0],
+        input.team.double?.[1]?.[1]
       ),
       this._addPlayer(
         preppedMap,
         based,
         teamed,
-        input.team.double[2][0],
-        input.team.double[2][1]
+        input.team.double?.[2]?.[0],
+        input.team.double?.[2]?.[1]
       ),
       this._addPlayer(
         preppedMap,
         based,
         teamed,
-        input.team.double[3][0],
-        input.team.double[3][1]
+        input.team.double?.[3]?.[0],
+        input.team.double?.[3]?.[1]
       ),
     ];
 
     const singles = [
-      this._addPlayer(preppedMap, based, teamed, input.team.single[0]).player1,
-      this._addPlayer(preppedMap, based, teamed, input.team.single[1]).player1,
-      this._addPlayer(preppedMap, based, teamed, input.team.single[2]).player1,
-      this._addPlayer(preppedMap, based, teamed, input.team.single[3]).player1,
+      this._addPlayer(preppedMap, based, teamed, input.team.single?.[0]).player1,
+      this._addPlayer(preppedMap, based, teamed, input.team.single?.[1]).player1,
+      this._addPlayer(preppedMap, based, teamed, input.team.single?.[2]).player1,
+      this._addPlayer(preppedMap, based, teamed, input.team.single?.[3]).player1,
     ];
 
     const subtitudes = input.team.subtitude.map(
