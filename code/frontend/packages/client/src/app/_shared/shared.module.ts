16a7eafb0982ae17c7e38abe2dd39542747dff58
Glenn <glenn.latomme@gmail.com>
2022-03-11 11:52:56 +0100

    feat(server): Update game when server saves

--- code/frontend/packages/client/src/app/_shared/shared.module.ts
+++ code/frontend/packages/client/src/app/_shared/shared.module.ts
@@ -1,5 +1,3 @@
-import { NgxMatDatetimePickerModule } from '@angular-material-components/datetime-picker';
-import { NgxMatMomentModule } from '@angular-material-components/moment-adapter';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -8,6 +6,7 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatOptionModule } from '@angular/material/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatExpansionModule } from '@angular/material/expansion';
@@ -19,7 +18,6 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatChipsModule } from '@angular/material/chips';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
@@ -32,17 +30,17 @@ import {
   NotificationComponent,
   PlayerSearchComponent,
   RankingShellComponent,
-  UserInfoComponent,
+  UserInfoComponent
 } from './components';
 import { BannerComponent } from './components/banner/banner.component';
 import { BetaComponent } from './components/beta/beta.component';
 import { ClaimComponent } from './components/claim/claim.component';
 import { ConfirmationDialogComponent } from './components/confirmation-dialog/confirmation-dialog.component';
 import { NewPlayerComponent } from './components/ranking-shell/components/new-player/new-player.component';
-import { HasClaimComponent } from './components/security/has-claim/has-claim.component';
-import { EnumToArrayPipe, LevelToLetterPipe, LoadingPipe } from './pipes';
-import { AssignRankingGroupsComponent } from './dialogs';
 import { WatchSystemInfoComponent } from './components/ranking-shell/components/watch-system-info/watch-system-info.component';
+import { HasClaimComponent } from './components/security/has-claim/has-claim.component';
+import { AssignRankingGroupsComponent } from './dialogs';
+import { EnumToArrayPipe, LevelToLetterPipe, LoadingPipe } from './pipes';
 
 const materialModules = [
   MatAutocompleteModule,
