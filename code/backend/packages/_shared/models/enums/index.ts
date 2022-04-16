f9548956ef42e2cc1b418f5429f5c833bd568075
Glenn <glenn.latomme@gmail.com>
2022-04-16 14:54:18 +0200

    fix: shared should always be called from root

--- code/backend/packages/_shared/models/enums/index.ts
+++ code/backend/packages/_shared/models/enums/index.ts
@@ -1,17 +1,17 @@
 // start:ng42.barrel
+export * from './changeEncounterAvailibilty.enum';
+export * from './clubMembershipType.enum';
 export * from './drawType.enum';
 export * from './eventType.enum';
+export * from './gameStatus.enum';
 export * from './gameType.enum';
 export * from './levelType.enum';
-export * from './subEventType.enum';
 export * from './rankingsSystemsType.enum';
-export * from './startingType.enum'
-export * from './rankingTiming.enum'
-export * from './usedRankingTiming.enum'
-export * from './teamMembershipType.enum'
-export * from './clubMembershipType.enum'
-export * from './useForTeams.enum'
-export * from './gameStatus.enum'
-export * from './changeEncounterAvailibilty.enum'
+export * from './rankingTiming.enum';
+export * from './startingType.enum';
+export * from './subEventType.enum';
+export * from './teamMembershipType.enum';
+export * from './usedRankingTiming.enum';
+export * from './useForTeams.enum';
 // end:ng42.barrel
 
