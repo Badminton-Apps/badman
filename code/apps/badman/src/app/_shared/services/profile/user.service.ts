434c8829620a1c3eb4a20868fe60b2ecd61e080b
Glenn Latomme <glenn.latomme@gmail.com>
2022-05-31 18:28:54 +0000

    pushing updates

--- code/apps/badman/src/app/_shared/services/profile/user.service.ts
+++ code/apps/badman/src/app/_shared/services/profile/user.service.ts
@@ -1,61 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, iif, Observable, of } from 'rxjs';
+import { distinctUntilChanged, filter, mergeMap, shareReplay, startWith, switchMap, tap } from 'rxjs/operators';
+import { RequestLink, Player } from '../../../_shared/models';
+import { environment } from '../../../../environments/environment';
 import { AuthService } from '@auth0/auth0-angular';
 import { ApmService } from '@elastic/apm-rum-angular';
-import { Apollo, gql } from 'apollo-angular';
-import { BehaviorSubject, combineLatest, iif, Observable, of } from 'rxjs';
-import {
-  distinctUntilChanged,
-  filter,
-  map,
-  mergeMap,
-  shareReplay,
-  tap,
-} from 'rxjs/operators';
-import { environment } from '../../../../environments/environment';
 import { apolloCache } from '../../../graphql.module';
-import { Player, RequestLink } from '../../../_shared/models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   private urlBase = `${environment.api}/api/${environment.apiVersion}/user`;
-  private update$ = new BehaviorSubject(null);
+  private updateHappend$ = new BehaviorSubject(null);
 
-  profile$: Observable<Player | null | undefined>;
+  profile$: Observable<{ player: Player; request: any } | { player: null; request: null } | null>;
   profile?: Player;
 
-  constructor(
-    private httpClient: HttpClient,
-    apollo: Apollo,
-    private auth: AuthService,
-    private apmService: ApmService
-  ) {
-    const whenAuthenticated = combineLatest([
-      apollo.query<{ me: Player }>({
-        query: gql`
-          query GetProfile {
-            me {
-              id
-              slug
-              fullName
-              claims {
-                name
-              }
-              clubs {
-                id
-                name
-                slug
-              }
-            }
-          }
-        `,
-      }),
-    ]).pipe(
-      map(([r]) => r.data?.me),
-      filter((me) => me !== null),
-      tap((me) => (this.profile = new Player(me) ?? undefined)),
+  constructor(private httpClient: HttpClient, private auth: AuthService, private apmService: ApmService) {
+    const whenAuthenticated = this.updateHappend$.pipe(
+      switchMap((_) => this.httpClient.get<{ player: Player; request: any }>(`${this.urlBase}/profile`)),
+      startWith({ player: null, request: null }),
+      filter((user) => user !== null),
+      tap(({ player }) => (this.profile = player ?? undefined)),
       shareReplay(1)
     );
 
@@ -67,16 +35,14 @@ export class UserService {
           id: user?.sub,
         });
       }),
-      mergeMap((x) =>
-        iif(() => x != null && x != undefined, whenAuthenticated, of(null))
-      ),
+      mergeMap((x) => iif(() => x != null && x != undefined, whenAuthenticated, of(null))),
       distinctUntilChanged(),
       shareReplay()
     );
   }
 
-  reloadProfile(): void {
-    this.update$.next(null);
+  profileUpdated() {
+    this.updateHappend$.next(null);
   }
 
   requestLink(playerId: string): Observable<RequestLink> {
@@ -86,11 +52,7 @@ export class UserService {
     );
   }
 
-  mergeAccoutns(
-    destination: string,
-    source: string[],
-    canBeDifferentMemberId: boolean
-  ): Observable<void> {
+  mergeAccoutns(destination: string, source: string[], canBeDifferentMemberId: boolean): Observable<void> {
     return this.httpClient
       .post<void>(`${this.urlBase}/merge-accounts`, {
         playerId: destination,
@@ -98,13 +60,10 @@ export class UserService {
         canBeDifferentMemberId,
       })
       .pipe(
-        tap(() => {
+        tap((_) => {
           // Clear from cache
           source.forEach((id) => {
-            const normalizedId = apolloCache.identify({
-              id,
-              __typename: 'Player',
-            });
+            const normalizedId = apolloCache.identify({ id, __typename: 'Player' });
             apolloCache.evict({ id: normalizedId });
           });
           apolloCache.gc();
