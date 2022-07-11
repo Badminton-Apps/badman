def39a74b320b122c87da462a7aec743e4809e9a
Glenn <glenn.latomme@gmail.com>
2022-07-11 14:52:10 +0200

    fix(ci): jest config wasn't configured correctly

--- code/libs/pupeteer/jest.config.js
+++ code/libs/pupeteer/jest.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-export default {
+module.exports = {
   displayName: 'pupeteer',
   preset: '../../jest.preset.js',
   globals: {
--- code/libs/ranking-calc/jest.config.js
+++ code/libs/ranking-calc/jest.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-export default {
+module.exports = {
   displayName: 'ranking-calc',
   preset: '../../jest.preset.js',
   globals: {
