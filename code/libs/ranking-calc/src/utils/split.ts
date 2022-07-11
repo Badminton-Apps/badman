dfed1db98f702391a1ea190a2920f543642e3051
Glenn <glenn.latomme@gmail.com>
2022-07-11 17:07:42 +0200

    feat(lib-ranking-calc): calculate update for all players
    All players that ever had an rankingplace should get an update when the simulation runs

--- code/libs/ranking-calc/src/utils/split.ts
+++ code/libs/ranking-calc/src/utils/split.ts
@@ -2,7 +2,10 @@ export const splitInChunks = (array, sizeChunks = 500) => {
   if (!Array.isArray(array)) {
     array = [array];
   }
+
+
   const newArray = [];
+
 
   while (array.length) {
     const chunk = array.splice(0, sizeChunks);
