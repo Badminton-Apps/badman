ab6c0d2375dfe67039781bfa672eb29fe60db773
Glenn <glenn.latomme@gmail.com>
2022-07-11 10:10:23 +0200

    build(api): :construction_worker: added helm charts

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,21 @@
-module.exports = {extends: ['./code/node_modules/@commitlint/config-conventional']}
+module.exports = {
+  extends: ['./code/node_modules/@commitlint/config-conventional'],
+  rules: {
+    "scope-enum": [
+      "api",
+      "client",
+      "scripts",
+      "worker-sync",
+      "worker-ranking",
+      "lib-authorization",
+      "lib-database",
+      "lib-generator",
+      "lib-mailing",
+      "lib-notification",
+      "lib-pupeteer",
+      "lib-queue",
+      "lib-search",
+      "lib-ranking-calc",
+    ],
+  }
+};
