8396bb690d4ece9674ab8bbc4317605e1d834d23
Glenn <glenn.latomme@gmail.com>
2022-07-11 14:52:10 +0200

    fix(ci): jest config wasn't configured correctly

--- code/apps/scripts/jest.config.js
+++ code/apps/scripts/jest.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-export default {
+module.exports = {
   displayName: 'scripts',
   preset: '../../jest.preset.js',
   globals: {
--- code/libs/queue/jest.config.js
+++ code/libs/queue/jest.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-export default {
+module.exports = {
   displayName: 'queue',
   preset: '../../jest.preset.js',
   globals: {
