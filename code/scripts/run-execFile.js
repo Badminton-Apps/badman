191d8d037086fb1ebfc94cb2912f8fa7d0bacf21
Glenn Latomme <glenn.latomme@gmail.com>
2022-07-12 14:56:38 +0200

    fix(ci): adding error reporting for standard version

--- code/scripts/run-execFile.js
+++ code/scripts/run-execFile.js
@@ -1,20 +1,16 @@
 // Credit: https://github.com/conventional-changelog/standard-version/blob/master/lib/run-execFile.js
 
 const { promisify } = require('util')
+const core = require('@actions/core');
 
 const execFile = promisify(require('child_process').execFile)
 
 module.exports = async function (args, cmd, cmdArgs) {
-  if (args.dryRun) return
-  try {
-    console.log(`Running: ${cmd} ${cmdArgs.join(' ')}`, args)
-    const { stderr, stdout } = await execFile(cmd, cmdArgs)
-    // If execFile returns content in stderr, but no error, print it as a warning
-    if (stderr) console.error(args, stderr)
-    return stdout
-  } catch (error) {
-    // If execFile returns an error, print it and exit with return code 1
-    console.error(args, error.stderr || error.message)
-    throw error
+  core.debug(`Running: ${cmd} ${cmdArgs.join(' ')}`, args)
+  const { stderr, stdout } = await execFile(cmd, cmdArgs)
+  // If execFile returns content in stderr, but no error, print it as a warning
+  if (stderr){
+    throw new Error(stderr)
   }
+  return stdout
 }
--- code/scripts/standard-version.js
+++ code/scripts/standard-version.js
@@ -1,6 +1,7 @@
 const standardVersion = require('standard-version');
 const runExecFile = require('./run-execFile');
 const runExec = require('./run-exec');
+const core = require('@actions/core');
 
 standardVersion({
   infile: 'apps/client/src/assets/CHANGELOG.md',
@@ -11,49 +12,54 @@ standardVersion({
   },
 })
   .then(async () => {
-    // get base and head from input args
-    const [, , base, head] = process.argv;
+    try {
+      // get base and head from input args
+      const [, , base, head] = process.argv;
 
-    // get the version number from the package.json
-    const pkg = require('../package.json');
-    const version = pkg.version;
-    const currentBranch = await runExecFile('', 'git', [
-      'rev-parse',
-      '--abbrev-ref',
-      'HEAD',
-    ]);
+      // get the version number from the package.json
+      const pkg = require('../package.json');
+      const version = pkg.version;
+      const currentBranch = await runExecFile('', 'git', [
+        'rev-parse',
+        '--abbrev-ref',
+        'HEAD',
+      ]);
 
-    // run `nx update helm` to update the helm chart
-    await runExec(
-      '',
-      `npx nx affected --target=update-version  --newVersion=${version} ${base} ${head}`
-    );
+      // run `nx update helm` to update the helm chart
+      await runExec(
+        '',
+        `npx nx affected --target=update-version  --newVersion=${version} ${base} ${head}`
+      );
 
-    // Git add .
-    await runExecFile('', 'git', ['add', '.']);
+      // Git add .
+      await runExecFile('', 'git', ['add', '.']);
 
-    // Git commit
-    await runExecFile('', 'git', [
-      'commit',
-      '-m',
-      `chore(release): ${version}`,
-    ]);
+      // Git commit
+      await runExecFile('', 'git', [
+        'commit',
+        '-m',
+        `chore(release): ${version}`,
+      ]);
 
-    // Git tag with annotation
-    await runExecFile('', 'git', [
-      'tag',
-      `v${version}`,
-      `-m`,
-      `chore(release): ${version}`,
-    ]);
+      // Git tag with annotation
+      await runExecFile('', 'git', [
+        'tag',
+        `v${version}`,
+        `-m`,
+        `chore(release): ${version}`,
+      ]);
 
-    // Git push
-    await runExecFile('', 'git', [
-      'push',
-      '--follow-tags',
-      'origin',
-      currentBranch.trim(),
-    ]);
+      // Git push
+      await runExecFile('', 'git', [
+        'push',
+        '--follow-tags',
+        'origin',
+        currentBranch.trim(),
+      ]);
+    } catch (err) {
+      core.error(err);
+      core.setFailed(`Action failed with error: ${err.message}`);
+    }
   })
   .catch((err) => {
     console.error(`standard-version failed with message: ${err.message}`);
