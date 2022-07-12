7bca3b0ada7a89453c39f947640b10691f43cf80
Glenn Latomme <glenn.latomme@gmail.com>
2022-07-12 14:56:38 +0200

    fix(ci): adding error reporting for standard version

--- code/scripts/run-exec.js
+++ code/scripts/run-exec.js
@@ -1,20 +1,16 @@
 // Credit: https://github.com/conventional-changelog/standard-version/blob/master/lib/run-exec.js
 
-const { promisify } = require('util')
+const { promisify } = require('util');
+const core = require('@actions/core');
 
-const exec = promisify(require('child_process').exec)
+const exec = promisify(require('child_process').exec);
 
 module.exports = async function (args, cmd) {
-  if (args.dryRun) return
-  try {
-    console.log(`Running: ${cmd}`, args)
-    const { stderr, stdout } = await exec(cmd)
-    // If exec returns content in stderr, but no error, print it as a warning
-    if (stderr) console.error(args, stderr)
-    return stdout
-  } catch (error) {
-    // If exec returns an error, print it and exit with return code 1
-    console.error(args, error.stderr || error.message)
-    throw error
+  core.debug(`Running: ${cmd}`, args);
+  const { stderr, stdout } = await exec(cmd);
+  // If exec returns content in stderr, but no error, print it as a warning
+  if (stderr) {
+    throw new Error(stderr);
   }
-}
+  return stdout;
+};
