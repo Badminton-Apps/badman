720dd45b7ac797458feba79ed76afedbde82c0c5
Glenn <glenn.latomme@gmail.com>
2022-07-15 11:34:25 +0200

    fix(api,worker-ranking,worker-sync): added redis password

--- code/libs/queue/src/queue.module.ts
+++ code/libs/queue/src/queue.module.ts
@@ -18,6 +18,7 @@ BullModule.registerQueueAsync;
         redis: {
           host: configService.get('REDIS_HOST'),
           port: +configService.get('REDIS_PORT'),
+          password: configService.get('REDIS_PASSWORD'),
         },
       }),
       inject: [ConfigService],
