793e499db5465e584d744b18e38be7020494dd34
Glenn <glenn.latomme@gmail.com>
2022-07-18 13:39:23 +0200

    fixup! 69ba85603d6f25fc39260e24bb06b647e7b38564

--- code/apps/scripts/src/main.ts
+++ code/apps/scripts/src/main.ts
@@ -1,19 +1,17 @@
-import { creatSequelizeInstance } from '@badman/api/database';
 import { Logger } from '@nestjs/common';
-import { FixGendersRunner } from './app/fix-ranking-genders';
 
 (async () => {
   const logger = new Logger('Scripts');
   logger.log('Starting script');
 
   logger.debug('Creating sequelize instance');
-  const sequelize = await creatSequelizeInstance();
+  // const sequelize = await creatSequelizeInstance();
 
   logger.debug('Creating Runner');
-  const genders = new FixGendersRunner(sequelize);
+  // const genders = new FixGendersRunner(sequelize);
 
   logger.debug('Processing');
-  await genders.process();
+  // await genders.process();
 
   logger.log('Done');
 })();
