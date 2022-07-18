69ba85603d6f25fc39260e24bb06b647e7b38564
Glenn <glenn.latomme@gmail.com>
2022-07-18 13:30:06 +0200

    feat(lib-api-database): make use of nestjs's sequelize module

--- code/libs/api/database/src/database.module.ts
+++ code/libs/api/database/src/database.module.ts
@@ -1,8 +1,62 @@
-import { Module } from '@nestjs/common';
-import { databaseProviders } from './database.providers';
-
+import { Logger, Module, OnModuleInit } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Model, ModelCtor } from 'sequelize-typescript';
+import * as sequelizeModels from './models';
+import { slugifyModel } from 'sequelize-slugify';
+import {
+  Club,
+  EventCompetition,
+  EventTournament,
+  Player,
+  Team,
+} from './models';
+import { Dialect } from 'sequelize';
 @Module({
-  providers: [...databaseProviders],
-  exports: [...databaseProviders],
+  imports: [
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        dialect: configService.get('DB_DIALECT'),
+        host: configService.get('DB_IP'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        models: Object.values(sequelizeModels).filter(
+          (m) => m.prototype instanceof Model
+        ) as ModelCtor[],
+        logging: configService.get('DB_LOGGING') === 'true',
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
-export class DatabaseModule {}
+export class DatabaseModule implements OnModuleInit {
+  private readonly logger = new Logger(DatabaseModule.name);
+
+  constructor() {
+    if (process.env.DB_DIALECT as Dialect) {
+      require('pg');
+    }
+  }
+
+  onModuleInit() {
+    this.logger.debug('initialize addons');
+    slugifyModel(Player as unknown as Model, {
+      source: ['firstName', 'lastName'],
+    });
+    slugifyModel(EventCompetition as unknown as Model, {
+      source: ['name'],
+    });
+    slugifyModel(EventTournament as unknown as Model, {
+      source: ['name'],
+    });
+    slugifyModel(Club as unknown as Model, {
+      source: ['name'],
+    });
+    slugifyModel(Team as unknown as Model, {
+      source: ['name'],
+    });
+  }
+}
