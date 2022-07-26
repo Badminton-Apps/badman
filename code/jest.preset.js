9517f36172bfdc616c2d8c4416d6ce18dd146cb0
Glenn Latomme <glenn.latomme@gmail.com>
2022-07-26 13:43:09 +0200

    fix(ci): jest needs some magic for uuid

--- code/jest.preset.js
+++ code/jest.preset.js
@@ -1,3 +1,8 @@
 const nxPreset = require('@nrwl/jest/preset').default;
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset, 
+  moduleNameMapper: {
+    '^uuid$': require.resolve('uuid'),
+  },
+};
