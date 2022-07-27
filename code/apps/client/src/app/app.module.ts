951bde74a62c3aad0fa1298c29f3abcabd57a63c
Glenn <glenn.latomme@gmail.com>
2022-07-27 10:58:33 +0200

    fixup! d3e96486ee6b59b278d2469cd9bdd0eee6112831

--- code/apps/client/src/app/app.module.ts
+++ code/apps/client/src/app/app.module.ts
@@ -146,6 +146,12 @@ const cookieConfig: NgcCookieConsentConfig = {
       },
     }),
     SocketModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     CookieService,
