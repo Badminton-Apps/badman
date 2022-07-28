b8e00dc15a305c8199065bf55c7fd9cf1defc7e6
Glenn <glenn.latomme@gmail.com>
2022-07-28 22:27:21 +0200

    fix(api): mailing wasn't enabled

--- code/libs/mailing/src/mailing.module.ts
+++ code/libs/mailing/src/mailing.module.ts
@@ -1,10 +1,11 @@
 import { DatabaseModule } from '@badman/api/database';
 import { HandlebarModule } from '@badman/handlebar';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MailingService } from './services';
 
 @Module({
-  imports: [HandlebarModule, DatabaseModule],
+  imports: [HandlebarModule, DatabaseModule, ConfigModule],
   providers: [MailingService],
   exports: [MailingService],
 })
