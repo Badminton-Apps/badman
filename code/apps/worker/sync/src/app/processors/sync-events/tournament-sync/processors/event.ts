086ea1b12d8be89f36c1995fcef4d31e9f537505
Glenn <glenn.latomme@gmail.com>
2022-08-01 15:08:45 +0200

    fix(worker-sync): setting visual code

--- code/apps/worker/sync/src/app/processors/sync-events/tournament-sync/processors/event.ts
+++ code/apps/worker/sync/src/app/processors/sync-events/tournament-sync/processors/event.ts
@@ -54,7 +54,10 @@ export class TournamentSyncEventProcessor extends StepProcessor {
       }).save({ transaction: this.transaction });
     } else {
       // Later we will change the search function to use the tournament code
-      if (event.visualCode === null) {
+      if (
+        event.visualCode === null ||
+        event.visualCode !== this.visualTournament.Code
+      ) {
         event.visualCode = this.visualTournament.Code;
         await event.save({ transaction: this.transaction });
       }
