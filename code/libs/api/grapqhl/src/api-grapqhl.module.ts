1d1c28be3cfeff3668e53b4ae36163d1d91b6faa
Glenn <glenn.latomme@gmail.com>
2022-08-05 11:01:54 +0200

    feat(api): caching grapqhl

--- code/libs/api/grapqhl/src/api-grapqhl.module.ts
+++ code/libs/api/grapqhl/src/api-grapqhl.module.ts
@@ -1,13 +1,18 @@
 import { ApiAuthorizationModule } from '@badman/api/authorization';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { BaseRedisCache } from 'apollo-server-cache-redis';
+import {
+  ApolloServerPluginCacheControl,
+  ApolloServerPluginLandingPageLocalDefault,
+} from 'apollo-server-core';
 import {
   ApolloServerPlugin,
   BaseContext,
   GraphQLRequestContextDidResolveOperation,
 } from 'apollo-server-plugin-base';
+import apm from 'elastic-apm-node';
 import Redis from 'ioredis';
 import { join } from 'path';
 import {
@@ -22,26 +27,33 @@ import {
   SecurityModule,
   TeamModule,
 } from './resolvers';
-import apm from 'elastic-apm-node';
-import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import responseCachePlugin from 'apollo-server-plugin-response-cache';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
+    GraphQLModule.forRoot({
       driver: ApolloDriver,
       playground: false,
       autoSchemaFile: join('schema/schema.gql'),
-      cache: new BaseRedisCache({
-        client: new Redis({
-          host: process.env.REDIS_HOST,
-          port: parseInt(process.env.REDIS_PORT, 10),
-          password: process.env.REDIS_PASSWORD,
-        }),
-      }),
+      cache: 'bounded',
       context: ({ request }) => {
         return { req: request };
       },
       plugins: [
+        responseCachePlugin({
+          cache: new BaseRedisCache({
+            client: new Redis({
+              host: process.env.REDIS_HOST,
+              port: parseInt(process.env.REDIS_PORT, 10),
+              password: process.env.REDIS_PASSWORD,
+              db: 1,
+            }),
+          }),
+        }),
+        ApolloServerPluginCacheControl({
+          defaultMaxAge: 60,
+          calculateHttpHeaders: true,
+        }),
         ApolloServerPluginLandingPageLocalDefault(),
         // Add the operation name to transaction
         (): ApolloServerPlugin => ({
@@ -73,6 +85,5 @@ import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
     GameModule,
     PlayerModule,
   ],
-  providers: [],
 })
 export class ApiGrapqhlModule {}
