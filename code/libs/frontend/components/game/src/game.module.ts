0ddc5b85996f6de64f3b7901aa0d489df7724087
Glenn <glenn.latomme@gmail.com>
2022-08-13 10:23:24 +0200

    fixup! 37ef182584ca8ad9322629da9a1d07469156c2a8

--- code/libs/frontend/components/game/src/game.module.ts
+++ code/libs/frontend/components/game/src/game.module.ts
@@ -4,7 +4,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
-import { PlayerInfoModule } from '@badman/frontend/components/player-info';
 import { SharedModule } from '@badman/frontend/shared';
 import { TranslateModule } from '@ngx-translate/core';
 import { MomentModule } from 'ngx-moment';
@@ -24,7 +23,6 @@ import {
   ],
   imports: [
     SharedModule,
-    PlayerInfoModule,
     MatExpansionModule,
     FlexModule,
     MomentModule,
