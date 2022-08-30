344feb7c4669ee3b439b8efbbdcbf6e5703c987a
Glenn Latomme <glenn.latomme@gmail.com>
2022-08-30 09:13:28 +0200

    fixup! 11c8a982af62becd61dac53127eb109185596a5c

--- code/apps/scripts/src/app/scripts/wrong-dates/wrong-dates.service.spec.ts
+++ code/apps/scripts/src/app/scripts/wrong-dates/wrong-dates.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WrongDatesService } from './wrong-dates.service';
+import { VisualModule } from '@badman/backend/visual';
 
 describe('WrongDatesService', () => {
   let service: WrongDatesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [VisualModule],
       providers: [WrongDatesService],
     }).compile();
 
