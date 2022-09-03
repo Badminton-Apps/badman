5a621734719957511ac93bff19dd58e2c9d8902a
Glenn <glenn.latomme@gmail.com>
2022-09-03 10:55:21 +0200

    feat(scripts): added option to manually import excel file with ranking + perform update base players

--- code/apps/scripts/src/app/app.module.ts
+++ code/apps/scripts/src/app/app.module.ts
@@ -3,18 +3,23 @@ import { VisualModule } from '@badman/backend/visual';
 import { Logger, Module, OnModuleInit } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ChangedEncountersService } from './scripts';
+import { ResyncBaseTeamsService } from './scripts/resync-base-teams/resync-base-teams.service';
 import { WrongDatesService } from './scripts/wrong-dates/wrong-dates.service';
 
 @Module({
-  providers: [ChangedEncountersService, WrongDatesService],
+  providers: [
+    ChangedEncountersService,
+    WrongDatesService,
+    ResyncBaseTeamsService,
+  ],
   imports: [ConfigModule.forRoot(), DatabaseModule, VisualModule],
 })
 export class ScriptModule implements OnModuleInit {
   private readonly logger = new Logger(ScriptModule.name);
-  constructor(private _changed: WrongDatesService) {}
+  constructor(private _changed: ResyncBaseTeamsService) {}
 
   async onModuleInit() {
     this.logger.log('Running script');
-    await this._changed.fixWrongDates(2022);
+    await this._changed.resyncBaseTeams();
   }
 }
