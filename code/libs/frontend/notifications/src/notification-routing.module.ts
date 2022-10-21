c27cc317485261f74b82a1f23f7168f34096b218
Glenn <glenn.latomme@gmail.com>
2022-10-21 17:05:55 +0200

    fix(frontend-notifications): auth guard for notifications settings

--- code/libs/frontend/notifications/src/notification-routing.module.ts
+++ code/libs/frontend/notifications/src/notification-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EditSettingsComponent } from './pages';
+import { AuthGuard } from '@badman/frontend-authentication';
 
-const routes: Routes = [{ path: '', component: EditSettingsComponent }];
+const routes: Routes = [
+  { path: '', component: EditSettingsComponent, canActivate: [AuthGuard] },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
