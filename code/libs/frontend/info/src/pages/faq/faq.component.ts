f831301bb994df0fbdf7e3967078338f97b562cc
Glenn <glenn.latomme@gmail.com>
2022-10-29 11:51:50 +0200

    feat(frontend-competition,frontend-info): adding titles

--- code/libs/frontend/info/src/pages/faq/faq.component.ts
+++ code/libs/frontend/info/src/pages/faq/faq.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.scss'],
 })
-export class FaqComponent {}
+export class FaqComponent {
+  constructor(titleService: Title) {
+    titleService.setTitle('FAQ');
+  }
+}
