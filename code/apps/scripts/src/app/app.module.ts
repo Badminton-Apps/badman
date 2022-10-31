b3baa4d10af3ff5552c66b5451e435039a8b39a5
Glenn <glenn.latomme@gmail.com>
2022-10-31 15:02:26 +0100

    chore(scripts): temporary placing this here for syncing

--- code/apps/scripts/src/app/app.module.ts
+++ code/apps/scripts/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { DatabaseModule } from '@badman/backend-database';
 import { Logger, Module, OnModuleInit } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { FillNotification } from './scripts/fill-notification-settings/wrong-dates.service';
+import { SyncRankingService } from './scripts/sync-ranking/sync-ranking.service';
 
 @Module({
-  providers: [FillNotification],
+  providers: [SyncRankingService],
   imports: [ConfigModule.forRoot(), DatabaseModule],
 })
 export class ScriptModule implements OnModuleInit {
   private readonly logger = new Logger(ScriptModule.name);
-  constructor(private fill: FillNotification) {}
+  constructor(private fill: SyncRankingService) {}
 
   async onModuleInit() {
     this.logger.log('Running script');
-    await this.fill.fixSettings();
+    const noRanking = await this.fill.findPlayersWithNoRanking();
+
+    this.logger.log(`Found ${noRanking.length} players with no ranking`);
+
+    for (const player of noRanking) {
+      // print progress % on 2 decimals
+      const index = noRanking.indexOf(player);
+      const progress = Math.round((index / noRanking.length) * 10000) / 100;
+      this.logger.log(`Progress: ${progress}%, ${index} / ${noRanking.length}`);
+
+      await this.fill.syncRanking(player.playerId);
+    }
   }
 }
---
+++ code/apps/scripts/src/app/scripts/sync-ranking/pupeteer/index.ts
@@ -0,0 +1,5 @@
+// start:ng42.barrel
+export * from './gotoPlayerPage';
+export * from './getRanking';
+export * from './gotoPlayerPageViaSearch';
+// end:ng42.barrel
