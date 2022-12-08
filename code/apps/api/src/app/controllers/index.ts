a9e979cab89ec5f049c7efa330c19b262bda88ff
Glenn <glenn.latomme@gmail.com>
2022-12-08 18:19:16 +0100

    chore(*): reverting to angular 14 and adding some server code

--- code/apps/api/src/app/controllers/index.ts
+++ code/apps/api/src/app/controllers/index.ts
@@ -2,4 +2,6 @@
 export * from './app.controller';
 export * from './pdf.controller';
 export * from './ranking.controller';
+export * from './image.controller';
+export * from './translation.controller';
 // end:ng42.barrel
