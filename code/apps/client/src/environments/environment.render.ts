72205a5921e34ca3b24c320dace78a205fd4c629
Glenn <glenn.latomme@gmail.com>
2022-12-29 14:39:35 +0100

    fix(client): update url to new endpoint

--- code/apps/client/src/environments/environment.render.ts
+++ code/apps/client/src/environments/environment.render.ts
@@ -2,7 +2,7 @@ import packages from '../version.json';
 
 export const environment = {
   production: true,
-  api: 'https://server-2msl.onrender.com',
+  api: 'https://api.badman.app',
   apiVersion: 'v1',
   apmServer: 'https://apm.badman.app',
   adsense: {
