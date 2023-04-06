d909c6da2888f512fc01d1e1addac998a5866318
Badman Releaser <info@badman.app>
2023-04-06 17:53:49 +0200

    fix: visual module

--- code/libs/backend/visual/src/visual.module.ts
+++ code/libs/backend/visual/src/visual.module.ts
@@ -1,39 +1,11 @@
-import { CacheStore, Module, CacheModule as nestCache } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-
-import { redisStore } from 'cache-manager-redis-store';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { VisualService } from './services';
+import { CacheModule } from '@badman/backend-cache';
 
 @Module({
-  imports: [
-    nestCache.registerAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => {
-        if (configService.get('DB_CACHE') === 'true') {
-          const redis = await redisStore({
-            socket: {
-              host: configService.get('REDIS_HOST'),
-              port: configService.get<number>('REDIS_PORT'),
-            },
-            password: configService.get('REDIS_PASSWORD'),
-            database: configService.get<number>('CACHE_DB') ?? 0,
-          }) as unknown as CacheStore;
-
-          return {
-            store: redis,
-            ttl: 60 * 60 * 24 * 7,
-          };
-        } else {
-          return {
-            ttl: 0,
-            store: 'memory',
-          };
-        }
-      },
-      inject: [ConfigService],
-    }),
-  ],
-  controllers: [],
-  providers: [],
-  exports: [nestCache],
+  imports: [CacheModule, ConfigModule],
+  providers: [VisualService],
+  exports: [VisualService],
 })
-export class CacheModule {}
+export class VisualModule {}
