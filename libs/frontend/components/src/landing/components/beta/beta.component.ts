e349801641333da3f046ab7a50350c309f18650f
Glenn Latomme <glenn.latomme@gmail.com>
2023-04-07 11:53:29 +0200

    feat: hide beta message

--- libs/frontend/components/src/landing/components/beta/beta.component.ts
+++ libs/frontend/components/src/landing/components/beta/beta.component.ts
@@ -9,11 +9,19 @@ import { MatCardModule } from '@angular/material/card';
 import { LoggedinUser, AuthenticateService } from '@badman/frontend-auth';
 import { Observable } from 'rxjs';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'badman-beta',
   standalone: true,
-  imports: [CommonModule, MatCardModule, RouterModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    RouterModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './beta.component.html',
   styleUrls: ['./beta.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -24,9 +32,22 @@ export class BetaComponent implements OnInit {
 
   user$?: Observable<LoggedinUser>;
 
+  // store the state of the beta message in local storage
+  hideBetaMessage = false;
+
   constructor(private authenticateService: AuthenticateService) {}
 
   ngOnInit() {
     this.user$ = this.authenticateService.user$;
+
+    const hideBetaMessage = localStorage.getItem('hideBetaMessage');
+    if (hideBetaMessage != undefined) {
+      this.hideBetaMessage = hideBetaMessage == 'true';
+    }
+  }
+
+  hideMessage() {
+    this.hideBetaMessage = true;
+    localStorage.setItem('hideBetaMessage', 'true');    
   }
 }
