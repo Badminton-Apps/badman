a2dd32cea4fc4afd954ce861822d18c45c23f4f0
Glenn Latomme <glenn.latomme@gmail.com>
2023-04-11 15:31:47 +0200

    feat: highlight ranking updates

---
+++ libs/backend/competition/enrollment/src/services/validate/index.ts
@@ -0,0 +1,4 @@
+// start:ng42.barrel
+export * from './rules';
+export * from './validation.service';
+// end:ng42.barrel
---
+++ libs/backend/competition/enrollment/src/services/validate/validation.service.ts
@@ -0,0 +1,91 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { AssemblyData, AssemblyOutput } from '../../models';
+import {
+  CompetitionStatusRule,
+  PlayerGenderRule,
+  PlayerMaxGamesRule,
+  PlayerMinLevelRule,
+  PlayerOrderRule,
+  Rule,
+  TeamBaseIndexRule,
+  TeamClubBaseRule,
+  TeamSubeventIndexRule,
+} from './rules';
+
+@Injectable()
+export class AssemblyService {
+  private readonly _logger = new Logger(AssemblyService.name);
+
+  async getValidationData(systemId: string): Promise<AssemblyData> {
+    return null;
+  }
+
+  /**
+   * Validate the assembly
+   *
+   * @param assembly Assembly configuaration
+   * @returns Whether the assembly is valid or not
+   */
+  async validate(
+    assembly: AssemblyData,
+    validators: Rule[]
+  ): Promise<AssemblyOutput> {
+    // get all errors and warnings from the validators in parallel
+    const results = await Promise.all(
+      validators.map((v) => v.validate(assembly))
+    );
+
+    const errors = results
+      ?.map((r) => r.errors)
+      ?.flat(1)
+      ?.filter((e) => !!e);
+    const warnings = results
+      ?.map((r) => r.warnings)
+      ?.flat(1)
+      ?.filter((e) => !!e);
+
+    return {
+      valid: errors.length === 0,
+      errors: errors,
+      warnings: warnings,
+      systemId: assembly.system.id,
+    };
+  }
+
+  async fetchAndValidate(
+    data: {
+      systemId: string;
+      teamId: string;
+      encounterId: string;
+
+      single1?: string;
+      single2?: string;
+      single3?: string;
+      single4?: string;
+
+      double1?: string[];
+      double2?: string[];
+      double3?: string[];
+      double4?: string[];
+
+      subtitudes?: string[];
+    },
+    validators: Rule[]
+  ) {
+    const dbData = await this.getValidationData(data.systemId);
+    return this.validate(dbData, validators);
+  }
+
+  static defaultValidators(): Rule[] {
+    return [
+      new PlayerOrderRule(),
+      new TeamBaseIndexRule(),
+      new TeamClubBaseRule(),
+      new TeamSubeventIndexRule(),
+      new CompetitionStatusRule(),
+      new PlayerMinLevelRule(),
+      new PlayerMaxGamesRule(),
+      new PlayerGenderRule(),
+    ];
+  }
+}
