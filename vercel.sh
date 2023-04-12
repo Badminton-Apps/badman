61c5b376e9109aa604c121d279aeefb6d0a609ea
Badman Releaser <info@badman.app>
2023-04-12 21:03:09 +0200

    feat: maybe this works

---
+++ vercel.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ 
+if [[ $VERCEL_GIT_COMMIT_REF == "main"  ]] ; then 
+  echo "This is our main branch"
+  yarn build badman:build --configuration prod
+else 
+  echo "This is not our main branch"
+  yarn build badman:build --configuration render
+fi
