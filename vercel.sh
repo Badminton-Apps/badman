c4cb3b651fac5cb3d68e03c8a418757265ee8480
Glenn Latomme <glenn.latomme@gmail.com>
2023-04-13 15:26:00 +0200

    fix: let us try this

--- vercel.sh
+++ vercel.sh
@@ -2,6 +2,7 @@
  
 if [[ $VERCEL_GIT_COMMIT_REF == "main"  ]] ; then 
   echo "This is our main branch"
+  cp ./apps/badman/src/manifest.prod.json ./apps/badman/src/manifest.json
   npm run build badman -- --configuration production
 else 
   echo "This is not our main branch"
