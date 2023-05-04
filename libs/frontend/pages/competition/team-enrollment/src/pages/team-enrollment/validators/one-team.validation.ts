7c3ce0bd17d88444caba227d377658de1daa4e4e
Glenn Latomme <glenn.latomme@gmail.com>
2023-05-04 12:59:18 +0200

    feat: some team validations

---
+++ libs/frontend/pages/competition/team-enrollment/src/pages/team-enrollment/validators/one-team.validation.ts
@@ -0,0 +1,17 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { Team } from '@badman/frontend-models';
+import { SubEventType } from '@badman/utils';
+
+export function minAmountOfTeams(teams: number): ValidatorFn {
+  return (
+    control: AbstractControl<{ [key in SubEventType]: Team[] }>
+  ): ValidationErrors | null => {
+    // we need in one of the keys to have at least one team
+    const teams = Object.values(control.value).reduce(
+      (acc, teams) => acc + teams.length,
+      0
+    );
+
+    return teams >= 1 ? null : { minAmountOfTeams: true };
+  };
+}
