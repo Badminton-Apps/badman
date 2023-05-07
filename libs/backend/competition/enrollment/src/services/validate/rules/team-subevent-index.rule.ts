ca6e4302d96c5643d221a5cbcab35b553f4c4d9b
Badman Releaser <info@badman.app>
2023-05-07 23:56:40 +0200

    fix: exemptions on the rule

--- libs/backend/competition/enrollment/src/services/validate/rules/team-subevent-index.rule.ts
+++ libs/backend/competition/enrollment/src/services/validate/rules/team-subevent-index.rule.ts
@@ -9,10 +9,18 @@ export class TeamSubeventIndexRule extends Rule {
   async validate(enrollment: EnrollmentValidationData) {
     const results = [] as RuleResult[];
 
-    for (const { baseIndex, subEvent, team } of enrollment.teams) {
+    for (const {
+      baseIndex,
+      subEvent,
+      team,
+      previousSeasonTeam,
+    } of enrollment.teams) {
       const errors = [] as EnrollmentValidationError[];
       const warnings = [] as EnrollmentValidationError[];
-      if (baseIndex < subEvent?.minBaseIndex) {
+      if (
+        baseIndex < subEvent?.minBaseIndex &&
+        !previousSeasonTeam?.entry?.standing?.faller
+      ) {
         errors.push({
           message: 'all.competition.team-enrollment.errors.team-to-strong',
           params: {
@@ -22,7 +30,10 @@ export class TeamSubeventIndexRule extends Rule {
         });
       }
 
-      if (baseIndex > subEvent?.maxBaseIndex) {
+      if (
+        baseIndex > subEvent?.maxBaseIndex &&
+        !previousSeasonTeam?.entry?.standing?.riser
+      ) {
         errors.push({
           message: 'all.competition.team-enrollment.errors.team-to-week',
           params: {
