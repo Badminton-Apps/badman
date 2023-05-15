7a1080bb564bc5c05ef8a567ed4fdc9ebd8e09d2
Glenn <glenn.latomme@gmail.com>
2023-05-15 10:45:59 +0200

    feat: privacy policy

---
+++ libs/frontend/components/src/privacy-policy/privacy-policy.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'badman-privacy-policy',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './privacy-policy.component.html',
+  styleUrls: ['./privacy-policy.component.scss'],
+})
+export class PrivacyPolicyComponent {}
