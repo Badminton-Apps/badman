5f77f416b5bc0de3405ecca0dc86b440ff7f5098
Badman Releaser <info@badman.app>
2023-05-18 11:11:45 +0200

    feat; first draft for enrollments

--- libs/frontend/pages/competition/event/src/pages/detail/competition-enrollments/competition-enrollments.component.ts
+++ libs/frontend/pages/competition/event/src/pages/detail/competition-enrollments/competition-enrollments.component.ts
@@ -1,4 +1,14 @@
 import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { CdkTableModule } from '@angular/cdk/table';
+import { CdkTreeModule } from '@angular/cdk/tree';
+import { CommonModule } from '@angular/common';
+import {
   Component,
   Injector,
   Input,
@@ -8,19 +18,47 @@ import {
   TransferState,
   inject,
 } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Apollo, gql } from 'apollo-angular';
-import { MatDialog } from '@angular/material/dialog';
-import { Team } from '@badman/frontend-models';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { MatCardModule } from '@angular/material/card';
+import { MatRippleModule } from '@angular/material/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTableModule } from '@angular/material/table';
+import { EventCompetition } from '@badman/frontend-models';
+import { Apollo, gql } from 'apollo-angular';
 import { map } from 'rxjs/operators';
-
+import { EnrollmentDetailRowDirective } from './competition-enrollments-detail.component';
+import { MatListModule } from '@angular/material/list';
 @Component({
   selector: 'badman-competition-enrollments',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatRippleModule,
+    MatListModule,
+
+    CdkTableModule,
+    CdkTreeModule,
+    EnrollmentDetailRowDirective,
+  ],
   templateUrl: './competition-enrollments.component.html',
   styleUrls: ['./competition-enrollments.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state(
+        'collapsed',
+        style({ height: '0px', minHeight: '0', visibility: 'hidden' })
+      ),
+      state('expanded', style({ height: '*', visibility: 'visible' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      ),
+    ]),
+  ],
 })
 export class CompetitionEnrollmentsComponent implements OnInit {
   // injects
@@ -31,25 +69,55 @@ export class CompetitionEnrollmentsComponent implements OnInit {
   dialog = inject(MatDialog);
 
   // signals
-  teams?: Signal<Team[] | undefined>;
+  eventCompetition?: Signal<EventCompetition | undefined>;
 
   // Inputs
   @Input({ required: true }) eventId?: string;
   @Input() season?: number;
+
+  displayedColumns: string[] = ['name'];
 
   ngOnInit(): void {
     this._setTeams();
   }
 
   private _setTeams(): void {
-    this.teams = toSignal(
+    this.eventCompetition = toSignal(
       this.apollo
-        .watchQuery<{ teams: Team[] }>({
+        .watchQuery<{ eventCompetition: Partial<EventCompetition> }>({
           query: gql`
             query EventEntries($eventCompetitionId: ID!) {
               eventCompetition(id: $eventCompetitionId) {
+                id
                 subEventCompetitions {
                   id
+                  name
+                  eventEntries {
+                    id
+                    team {
+                      id
+                      name
+                      club {
+                        id
+                        name
+                      }
+                    }
+                    meta {
+                      competition {
+                        teamIndex
+                        players {
+                          id
+                          player {
+                            id
+                            fullName
+                          }
+                          single
+                          double
+                          mix
+                        }
+                      }
+                    }
+                  }
                 }
               }
             }
@@ -58,7 +126,10 @@ export class CompetitionEnrollmentsComponent implements OnInit {
             eventCompetitionId: this.eventId,
           },
         })
-        .valueChanges.pipe(map((result) => result.data.teams))
+        .valueChanges.pipe(
+          map((result) => new EventCompetition(result.data.eventCompetition))
+        ),
+      { injector: this.injector }
     );
   }
 }
