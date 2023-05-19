cdf629d3095339cc41530ba915e3ac6a6bf6937d
Badman Releaser <info@badman.app>
2023-05-19 11:37:12 +0200

    remove log

--- libs/frontend/pages/competition/event/src/pages/detail/detail.page.ts
+++ libs/frontend/pages/competition/event/src/pages/detail/detail.page.ts
@@ -148,7 +148,6 @@ export class DetailPageComponent implements OnInit {
           map((params) => params['tab'])
         )
         .subscribe((tabindex) => {
-          console.log('tabindex', tabindex);
           this.currentTab.set(parseInt(tabindex, 10));
         });
 
