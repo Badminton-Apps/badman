390f7fc106214f55cf186cb4eebcb89b9005e435
Badman Releaser <info@badman.app>
2023-06-03 15:12:04 +0200

    ci: fastify defaults on localhost not 0.0.0.0

--- apps/worker/sync/src/main.ts
+++ apps/worker/sync/src/main.ts
@@ -6,6 +6,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     WorkerSyncModule,
@@ -15,7 +16,13 @@ async function bootstrap() {
     }
   );
 
+  const configService = app.get<ConfigService>(ConfigService);
+  const port = configService.get('PORT') || 5001;
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
-  app.listen(5001);
+  await app.listen(port, '0.0.0.0', (error) => {
+    if (error) {
+      process.exit(1);
+    }
+  });
 }
 bootstrap();
