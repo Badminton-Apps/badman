bd32a955f3c3bbf99ac66e6db798c97a087045d5
Badman Releaser <info@badman.app>
2023-06-03 10:43:29 +0200

    chore: fastify workers and buffer logs

--- apps/worker/ranking/src/main.ts
+++ apps/worker/ranking/src/main.ts
@@ -6,11 +6,19 @@
 import { NestFactory } from '@nestjs/core';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { WorkerRankingModule } from './app/app.module';
-
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(WorkerRankingModule, {
-    bufferLogs: true,
-  });
+  const app = await NestFactory.create<NestFastifyApplication>(
+    WorkerRankingModule,
+    new FastifyAdapter(),
+    {
+      bufferLogs: true,
+    }
+  );
+
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 }
 bootstrap();
--- apps/worker/sync/src/main.ts
+++ apps/worker/sync/src/main.ts
@@ -2,11 +2,19 @@ import { WorkerSyncModule } from './app/app.module';
 
 import { NestFactory } from '@nestjs/core';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
-
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(WorkerSyncModule, {
-    bufferLogs: true,
-  });
+  const app = await NestFactory.create<NestFastifyApplication>(
+    WorkerSyncModule,
+    new FastifyAdapter(),
+    {
+      bufferLogs: true,
+    }
+  );
+
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 }
 bootstrap();
