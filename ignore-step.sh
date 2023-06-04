5e3fa04c9f25471a996b3380152f910d202d353f
Badman Releaser <info@badman.app>
2023-06-04 16:15:42 +0200

    fix: only build release

---
+++ ignore-step.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Only build if it's a release commit
+
+echo "VERCEL_GIT_COMMIT_MESSAGE: $VERCEL_GIT_COMMIT_MESSAGE"
+echo "VERCEL_GIT_COMMIT_REF $VERCEL_GIT_COMMIT_REF"
+
+# if the message doens't have chore(xxx) on the main or develop branch, then we want to skip the build
+if [[ "$VERCEL_GIT_COMMIT_MESSAGE" == *"chore(release)"* ]] ; then
+  # Proceed with the build
+  echo "✅ - Build can proceed"
+  exit 1;
+else
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+fi
+
