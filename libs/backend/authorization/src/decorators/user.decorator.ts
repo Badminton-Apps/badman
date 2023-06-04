0bcb89cea564f711a546cdea2446f18d1b72feb9
Badman Releaser <info@badman.app>
2023-06-04 18:50:53 +0200

    fix: get requets for both contexts

--- libs/backend/authorization/src/decorators/user.decorator.ts
+++ libs/backend/authorization/src/decorators/user.decorator.ts
@@ -1,12 +1,12 @@
 import { Player } from '@badman/backend-database';
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { GqlExecutionContext } from '@nestjs/graphql';
 import { env } from 'process';
-
+import { getRequest } from '../utils';
 export const User = createParamDecorator(
   async (data: unknown, context: ExecutionContext) => {
-    const ctx = GqlExecutionContext.create(context);
-    const user = ctx.getContext().req?.['user'];
+    const request = getRequest(context);
+
+    const user = request['user'];
 
     // If we have a user in the request, return it
     if (user && user.sub) {
