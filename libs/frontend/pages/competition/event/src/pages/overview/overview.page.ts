e93cb21e42755bfb1e397d7126a3f78486fba592
cskiwi <glenn.latomme@gmail.com>
2023-07-02 10:19:29 +0200

    feat: Competition to event selection

--- libs/frontend/pages/competition/event/src/pages/overview/overview.page.ts
+++ libs/frontend/pages/competition/event/src/pages/overview/overview.page.ts
@@ -1,15 +1,5 @@
 import { CommonModule } from '@angular/common';
-import {
-  Component,
-  Injector,
-  OnInit,
-  PLATFORM_ID,
-  Signal,
-  TransferState,
-  inject,
-  signal,
-} from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
@@ -22,12 +12,11 @@ import {
   AddEventComponent,
   HasClaimComponent,
   PageHeaderComponent,
+  SelectSeasonComponent,
 } from '@badman/frontend-components';
 import { JobsService } from '@badman/frontend-jobs';
-import { transferState } from '@badman/frontend-utils';
 import { getCurrentSeason } from '@badman/utils';
 import { TranslateModule } from '@ngx-translate/core';
-import { Apollo, gql } from 'apollo-angular';
 import { MomentModule } from 'ngx-moment';
 import { lastValueFrom } from 'rxjs';
 import { filter, map, take } from 'rxjs/operators';
@@ -58,15 +47,10 @@ import { CompetitionEventsComponent } from './competition-events/competition-eve
     PageHeaderComponent,
     HasClaimComponent,
     CompetitionEventsComponent,
+    SelectSeasonComponent,
   ],
 })
 export class OverviewPageComponent implements OnInit {
-  // injects
-  apollo = inject(Apollo);
-
-  injector = inject(Injector);
-  stateTransfer = inject(TransferState);
-  platformId = inject(PLATFORM_ID);
   route = inject(ActivatedRoute);
   router = inject(Router);
 
@@ -77,7 +61,6 @@ export class OverviewPageComponent implements OnInit {
   snackBar = inject(MatSnackBar);
 
   // signals
-  seasons?: Signal<number[]>;
   currentTab = signal(0);
 
   // other
@@ -87,8 +70,6 @@ export class OverviewPageComponent implements OnInit {
     this.filter = this.formBuilder.group({
       season: getCurrentSeason(),
     });
-
-    this._setYears();
 
     // check if the query params contian tabindex
     this.route.queryParams
@@ -100,38 +81,6 @@ export class OverviewPageComponent implements OnInit {
       .subscribe((tabindex) => {
         this.currentTab.set(parseInt(tabindex, 10));
       });
-  }
-
-  private _setYears() {
-    this.seasons = toSignal(
-      this.apollo
-        .query<{
-          eventCompetitionSeasons: number[];
-        }>({
-          query: gql`
-            query CompetitionYearsCompetition {
-              eventCompetitionSeasons
-            }
-          `,
-        })
-        .pipe(
-          transferState(
-            `eventCompetitions-seasons`,
-            this.stateTransfer,
-            this.platformId
-          ),
-          map((result) => {
-            if (!result?.data.eventCompetitionSeasons) {
-              throw new Error('No teams');
-            }
-            return result.data.eventCompetitionSeasons;
-          })
-        ),
-      {
-        initialValue: [getCurrentSeason()],
-        injector: this.injector,
-      }
-    );
   }
 
   async addEvent() {
