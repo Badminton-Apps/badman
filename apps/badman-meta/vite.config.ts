7426af9144b58272cf45d9d3037fe1990d7157b1
cskiwi <glenn.latomme@gmail.com>
2023-07-03 20:57:04 +0200

    feat: adding badman project as meta

---
+++ apps/badman-meta/vite.config.ts
@@ -0,0 +1,53 @@
+/// <reference types="vitest" />
+
+import analog from '@analogjs/platform';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import tsConfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const nitro =
+    mode === 'production'
+      ? {
+          output: {
+            dir: '../../../../.vercel/output',
+            publicDir: '../../../../.vercel/output/static',
+          },
+        }
+      : undefined;
+
+  return {
+    publicDir: 'src/public',
+    optimizeDeps: {
+      include: ['@angular/common', '@angular/forms', 'isomorphic-fetch'],
+    },
+    ssr: {
+      noExternal: '@analogjs/trpc',
+    },
+
+    build: {
+      target: ['es2020'],
+    },
+    plugins: [
+      analog({
+        nitro,
+      }),
+      tsConfigPaths({
+        root: '../../',
+      }),
+      splitVendorChunkPlugin(),
+    ],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['src/test-setup.ts'],
+      include: ['**/*.spec.ts'],
+      cache: {
+        dir: `../../node_modules/.vitest`,
+      },
+    },
+    define: {
+      'import.meta.vitest': mode !== 'production',
+    },
+  };
+});
