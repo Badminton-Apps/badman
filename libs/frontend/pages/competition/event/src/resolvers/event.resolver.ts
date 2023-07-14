45380a02a6216a890765c52ca0e095a54ca8a228
cskiwi <glenn.latomme@gmail.com>
2023-07-14 14:55:19 +0200

    fix: fetch close request date

--- libs/frontend/pages/competition/event/src/resolvers/event.resolver.ts
+++ libs/frontend/pages/competition/event/src/resolvers/event.resolver.ts
@@ -29,6 +29,7 @@ export class EventResolver {
               closeDate
               changeOpenDate
               changeCloseDate
+              changeCloseRequestDate
               visualCode
               official
               lastSync
