fdf2355ca3e9be676996a392e0a878ff1431566c
cskiwi <glenn.latomme@gmail.com>
2023-07-17 13:14:41 +0200

    feat: re-creating calendar to be more fluent

--- libs/frontend/pages/competition/change-encounter/src/index.ts
+++ libs/frontend/pages/competition/change-encounter/src/index.ts
@@ -3,5 +3,3 @@ export * from './change-encounter.module';
 export * from './pages';
 export * from './components';
 // end:ng42.barrel
-
-export * from './pages/change-encounter/components/request-date/request-date.component';
