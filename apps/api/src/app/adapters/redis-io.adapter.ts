313f85b4e617f906ca843129390be31b1b7bc253
cskiwi <glenn.latomme@gmail.com>
2023-07-19 11:04:35 +0200

    fix: triggering change for testing new build

--- apps/api/src/app/adapters/redis-io.adapter.ts
+++ apps/api/src/app/adapters/redis-io.adapter.ts
@@ -1,7 +1,7 @@
 import { IoAdapter } from '@nestjs/platform-socket.io';
-import { ServerOptions } from 'socket.io';
 import { createAdapter } from '@socket.io/redis-adapter';
 import { createClient } from 'redis';
+import { ServerOptions } from 'socket.io';
 
 export class RedisIoAdapter extends IoAdapter {
   private adapterConstructor?: ReturnType<typeof createAdapter>;
