979509237e335541bc27ef72ecc7fd1a4765920a
cskiwi <glenn.latomme@gmail.com>
2023-07-28 12:51:34 +0200

    fix: require login

--- libs/frontend/pages/competition/change-encounter/src/change-encounter.module.ts
+++ libs/frontend/pages/competition/change-encounter/src/change-encounter.module.ts
@@ -2,13 +2,24 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ChangeEncounterComponent } from './pages';
+import { AuthGuard } from '@badman/frontend-auth';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     RouterModule.forChild([
-      { path: '', pathMatch: 'full', component: ChangeEncounterComponent },
+      {
+        path: '',
+        pathMatch: 'full',
+        component: ChangeEncounterComponent,
+        canActivate: [AuthGuard],
+        data: {
+          claims: {
+            any: ['[:id]_change:encounter', 'change-any:encounter'],
+          },
+        },
+      },
     ]),
   ],
 })
