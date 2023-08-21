0a3a1f8d59ef9c9580b60c73ef9a09e4bee528b5
cskiwi <glenn.latomme@gmail.com>
2023-08-21 16:54:34 +0200

    feat: script for detecting changed encounters

--- apps/scripts/src/app/scripts/incorrect-change-encounters/incorrect-changed-encounters.service.ts
+++ apps/scripts/src/app/scripts/incorrect-change-encounters/incorrect-changed-encounters.service.ts
@@ -1,13 +1,14 @@
 import {
   EncounterChange,
+  EncounterChangeDate,
   EncounterCompetition,
   Team,
 } from '@badman/backend-database';
 import { ChangeEncounterAvailability } from '@badman/utils';
 import { Injectable, Logger } from '@nestjs/common';
-import { writeFile } from 'fs/promises';
 import moment from 'moment';
 import { Op } from 'sequelize';
+import * as XLSX from 'xlsx';
 
 @Injectable()
 export class IncorrectEncountersService {
@@ -15,50 +16,137 @@ export class IncorrectEncountersService {
 
   async getIncorrectEncountersService(season: number) {
     this.logger.debug('getIncorrectEncountersService');
-    const startDate = moment([season, 9, 1]).toDate();
+    const startDate = moment([season, 8, 1]).toDate();
     const endDate = moment([season + 1, 7, 1]).toDate();
 
     const encounters = await EncounterCompetition.findAll({
-      attributes: ['id'],
+      attributes: ['id', 'date', 'originalDate', 'accepted'],
       where: {
+        originalDate: {
+          [Op.ne]: null,
+        },
         date: {
           [Op.between]: [startDate, endDate],
         },
       },
       include: [
         { attributes: ['id', 'name', 'clubId'], model: Team, as: 'home' },
-        { attributes: ['name'], model: Team, as: 'away' },
+        { attributes: ['id', 'name', 'clubId'], model: Team, as: 'away' },
         {
-          attributes: ['id', 'dates'],
+          attributes: ['id'],
           model: EncounterChange,
+          include: [
+            {
+              model: EncounterChangeDate,
+            },
+          ],
           required: true,
         },
       ],
     });
 
-    // Write to csv file
-    const csv =
-      `home,away,link,dates\n` +
-      encounters
-        .map((encounter) => {
-          const dates = encounter.encounterChange?.dates?.filter(
-            (d) =>
-              d.availabilityAway == ChangeEncounterAvailability.POSSIBLE &&
-              d.availabilityHome == ChangeEncounterAvailability.POSSIBLE
-          );
+    let indexWithMostColumns = 0;
+    let maxColumns = 0;
 
-          return `${encounter.home?.name},${
-            encounter.away?.name
-          },https://badman.app/competition/change-encounter?club=${
-            encounter.home?.clubId
-          }&team=${encounter.home?.id}&encounter=${encounter.id},${
-            encounter.date == encounter.originalDate
-          },${dates?.map((d) => d.date).join(',') ?? ''}`; 
-        })
-        .join('\n');
-    const fileName = `${season}-incorrect-changed-encounters.csv`;
+    const data = [
+      [
+        'Home Team',
+        'Away Team',
+        'Accepted',
+        'link',
+        'Current date',
+        'Original date',
+        'Probably wrong',
+        '# Dates',
+        'Suggestions',
+      ],
+      ...encounters.map((encounter) => {
+        const dates = encounter.encounterChange?.dates?.filter(
+          (d) =>
+            d.availabilityAway === ChangeEncounterAvailability.POSSIBLE &&
+            d.availabilityHome === ChangeEncounterAvailability.POSSIBLE
+        );
 
-    await writeFile(fileName, csv);
+        // Home team
+        const home = encounter.home?.name ?? '';
+        // Away team
+        const away = encounter.away?.name ?? '';
+
+        // Link
+        const link = {
+          t: 'external',
+          v: `open in badman`,
+          l: {
+            Target: `http://localhost:3000/competition/change-encounter?club=${encounter.home?.clubId}&team=${encounter.home?.id}&encounter=${encounter.id}`,
+            Tooltip: `Open in Badman`,
+          },
+        };
+
+        // Current date
+        if (!encounter.date) {
+          throw new Error('No date found');
+        }
+        const currentDate = new Date(encounter.date).toLocaleString();
+
+        // Original date
+        if (!encounter.originalDate) {
+          throw new Error('No original date found');
+        }
+        const originalDate = new Date(encounter.originalDate).toLocaleString();
+
+        // Probably wrong
+        const probablyWrong = moment(encounter.date).isSame(encounter.originalDate, 'minute');
+
+        // Dates
+        const datesCount = dates?.length ?? 0;
+
+        // Suggestions
+        const suggestions =
+          dates?.map((d) => new Date(d.date!).toLocaleString()) ?? [];
+
+        if (suggestions.length > maxColumns) {
+          maxColumns = suggestions.length;
+          indexWithMostColumns = encounters.indexOf(encounter) + 1;
+        }
+
+        return [
+          home,
+          away,
+          encounter.accepted,
+          link,
+          currentDate,
+          originalDate,
+          probablyWrong,
+          datesCount,
+          ...suggestions,
+        ];
+      }),
+    ];
+
+    const ws = XLSX.utils.aoa_to_sheet(data);
+
+    // Autosize columns
+    const columnSizes = data[indexWithMostColumns].map((_, columnIndex) =>
+      data.reduce(
+        (acc, row) => Math.max(acc, (`${row[columnIndex]}`.length ?? 0) + 2),
+        0
+      )
+    );
+    ws['!cols'] = columnSizes.map((width) => ({ width }));
+
+    // Enable filtering
+    ws['!autofilter'] = {
+      ref: XLSX.utils.encode_range(
+        XLSX.utils.decode_range(ws['!ref'] as string)
+      ),
+    };
+
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    const fileName = `${season}-incorrect-changed-encounters.xlsx`;
+
+    // Save the Excel file
+    XLSX.writeFile(wb, fileName);
 
     this.logger.log(`Found ${encounters.length} changed encounters`);
   }
