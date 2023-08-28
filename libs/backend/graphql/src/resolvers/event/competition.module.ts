1f9ecbc7f43eaf2482249cc9bab65763e91edb9f
cskiwi <glenn.latomme@gmail.com>
2023-08-28 10:59:26 +0200

    fix: moving to sync queue

--- libs/backend/graphql/src/resolvers/event/competition.module.ts
+++ libs/backend/graphql/src/resolvers/event/competition.module.ts
@@ -1,21 +1,20 @@
 import { AssemblyModule } from '@badman/backend-assembly';
+import { CacheModule } from '@badman/backend-cache';
 import { DatabaseModule } from '@badman/backend-database';
+import { EnrollmentModule } from '@badman/backend-enrollment';
 import { NotificationsModule } from '@badman/backend-notifications';
 import { QueueModule } from '@badman/backend-queue';
 import { RankingModule } from '@badman/backend-ranking';
 import { Module } from '@nestjs/common';
 import {
+  AssemblyResolver,
   DrawCompetitionResolver,
   EncounterChangeCompetitionResolver,
   EncounterCompetitionResolver,
+  EnrollmentResolver,
   EventCompetitionResolver,
   SubEventCompetitionResolver,
-  AssemblyResolver,
-  EnrollmentResolver,
 } from './competition';
-import { CacheModule } from '@badman/backend-cache';
-import { EnrollmentModule } from '@badman/backend-enrollment';
-import { VisualModule } from '@badman/backend-visual';
 
 @Module({
   imports: [
@@ -26,7 +25,6 @@ import { VisualModule } from '@badman/backend-visual';
     EnrollmentModule,
     RankingModule,
     CacheModule,
-    VisualModule,
   ],
   providers: [
     EventCompetitionResolver,
