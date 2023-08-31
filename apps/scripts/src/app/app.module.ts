23b1c44f194d783f03decdcdaa11a932ec2a0592
cskiwi <glenn.latomme@gmail.com>
2023-08-31 16:13:45 +0200

    feat: script for exporting encounters with locations

--- apps/scripts/src/app/app.module.ts
+++ apps/scripts/src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { DatabaseModule } from '@badman/backend-database';
+import { GeneratorModule } from '@badman/backend-generator';
 import { Logger, Module, OnModuleInit } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { IncorrectEncountersService } from './scripts/incorrect-change-encounters';
-import { VisualModule } from '@badman/backend-visual';
+import { AddLocationsId } from './scripts/add-locations-to-cp';
 
 @Module({
-  providers: [IncorrectEncountersService],
-  imports: [ConfigModule.forRoot(), DatabaseModule, VisualModule],
+  providers: [AddLocationsId],
+  imports: [ConfigModule.forRoot(), DatabaseModule, GeneratorModule],
 })
 export class ScriptModule implements OnModuleInit {
   private readonly logger = new Logger(ScriptModule.name);
-  constructor(private fixer: IncorrectEncountersService) {}
+  constructor(private fixer: AddLocationsId) {}
 
   async onModuleInit() {
     this.logger.log('Running script');
-    await this.fixer.sendEncountersToVisual(2023);
+
+    await this.fixer.encountersWithLocations('2625c52f-fae4-4a81-87ed-6819f44f1dcf');
 
     this.logger.log('Script finished');
   }
