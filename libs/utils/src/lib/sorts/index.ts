3cff230e6d781b24b7f459893c39cc3401448c26
cskiwi <glenn.latomme@gmail.com>
2023-09-15 09:36:36 +0200

    fix: sort encounters

--- libs/utils/src/lib/sorts/index.ts
+++ libs/utils/src/lib/sorts/index.ts
@@ -1,5 +1,6 @@
 // start:ng42.barrel
 export * from './comments.sort';
+export * from './encounters.sort';
 export * from './games.sort';
 export * from './players.sort';
 export * from './standing.sort';
