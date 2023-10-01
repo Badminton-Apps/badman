d524c01b0ef2525cd93c92e3842cddc4ca87a702
cskiwi <glenn.latomme@gmail.com>
2023-10-01 17:19:27 +0200

    feat: convert difference to decimal

---
+++ database/migrations/20231001142715-rankingpoints can be a decimal.js
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, sequelize) => {
+    return queryInterface.sequelize.transaction(async (t) => {
+      try {
+        await queryInterface.changeColumn(
+          {
+            schema: 'ranking',
+            tableName: 'RankingPoints',
+          },
+          'differenceInLevel',
+          {
+            type: sequelize.DataTypes.DECIMAL(10, 2),
+          },
+          { transaction: t }
+        );
+      } catch (err) {
+        console.error('We errored with', err?.message ?? err);
+        t.rollback();
+      }
+    });
+  },
+
+  down: async (queryInterface) => {
+    return queryInterface.sequelize.transaction(async (t) => {
+      try {
+        await queryInterface.changeColumn(
+          {
+            schema: 'ranking',
+            tableName: 'RankingPoints',
+          },
+          'differenceInLevel',
+          {
+            type: sequelize.DataTypes.INTEGER,
+          },
+          { transaction: t }
+        );
+      } catch (err) {
+        console.error('We errored with', err);
+        t.rollback();
+      }
+    });
+  },
+};
