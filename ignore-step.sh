cb6f607299f9d006bca38fc2c0c0eef9d80122ce
Glenn Latomme <glenn.latomme@gmail.com>
2023-10-05 09:41:55 +0200

    ci: testing more stuff

--- ignore-step.sh
+++ ignore-step.sh
@@ -1,10 +1,10 @@
+echo "VERCEL_GIT_COMMIT_MESSAGE: $VERCEL_GIT_COMMIT_MESSAGE"
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "main" && "$VERCEL_GIT_COMMIT_MESSAGE" =~ "chore(release)" ]]; then
   # Proceed with the build
-    echo "✅ - Build can proceed"
+  echo "✅ - Build can proceed"
   exit 1;
-
 else
   # Don't build
   echo "🛑 - Build cancelled"
