9949f1b5efe43cf87bf7c3c08f5ee3c9be2bdd6d
cskiwi <glenn.latomme@gmail.com>
2023-10-07 13:48:33 +0200

    test: some tests, still some work on the drag and drop

--- apps/badman-e2e/src/tests/assembly.e2e.spec.ts
+++ apps/badman-e2e/src/tests/assembly.e2e.spec.ts
@@ -11,19 +11,49 @@ test.describe('Assembly page', () => {
     await expect(page.locator('h1')).toContainText('Team assembly');
   });
 
-  test('We can select encounter', async ({ page }) => {
-    const selectClub = page.locator('badman-select-club input');
+  test.describe('assembly', () => {
+    test.beforeEach(async ({ page }) => {
+      const selectClub = page.locator('badman-select-club input');
 
-    await selectClub.fill('BC Broodrooster');
-    await page.keyboard.press('ArrowDown');
-    await page.keyboard.press('Enter');
+      await selectClub.fill('BC Broodrooster');
+      await page.keyboard.press('ArrowDown');
+      await page.keyboard.press('Enter');
 
-    const selectTeam = page.locator('badman-select-team');
-    await selectTeam.click();
-    await page.keyboard.press('ArrowDown');
-    await page.keyboard.press('Enter');
+      const selectTeam = page.locator('badman-select-team');
+      await selectTeam.click();
+      await page.keyboard.press('ArrowDown');
+      await page.keyboard.press('Enter');
+    });
 
+    test('We can select encounter', async ({ page }) => {
+      await expect(page.locator('badman-assembly')).toBeVisible();
+    });
 
-    await expect(page.locator('badman-assembly')).toBeVisible();
+    test('Players of team have been loaded', async ({ page }) => {
+      const player888M = page
+        .locator('badman-assembly-player')
+        .filter({ hasText: 'M 8-8-8' });
+
+      await expect(player888M).toBeVisible();
+    });
+
+    // test('We can drage a male to the males dubbles', async ({ page }) => {
+    //   const malesDoubles = page.locator('#double1List');
+    //   const player888M = page
+    //     .locator('badman-assembly-player')
+    //     .filter({ hasText: 'M 8-8-8' })
+    //     .locator('div')
+    //     .first();
+
+    //   await expect(player888M).toBeVisible();
+    //   await expect(malesDoubles).toBeVisible();
+
+    //   await player888M.dragTo(malesDoubles, {
+    //     sourcePosition: { x: 10, y: 20 },
+    //     targetPosition: { x: 10, y: 20 },
+    //   });
+
+    //   // await expect(malesDoubles).toContainText('M 8-8-8 BC Broodrooster');
+    // });
   });
 });
