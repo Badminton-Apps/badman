d08020ca40a535d81090833401e1a80a43a3deb3
cskiwi <glenn.latomme@gmail.com>
2023-10-07 13:16:31 +0200

    feat: ilike doesn't exist on sqlite

--- libs/backend/search/src/search.module.ts
+++ libs/backend/search/src/search.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SearchService } from './services';
 import { SearchResolver } from './services';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [],
   providers: [SearchService, SearchResolver],
   exports: [],
