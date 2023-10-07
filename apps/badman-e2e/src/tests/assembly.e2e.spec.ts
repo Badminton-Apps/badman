2ee7a2b2286f07c7e985ab99c77197c011ec9a11
cskiwi <glenn.latomme@gmail.com>
2023-10-07 13:29:59 +0200

    test: check if the assembly get's loaded

--- apps/badman-e2e/src/tests/assembly.e2e.spec.ts
+++ apps/badman-e2e/src/tests/assembly.e2e.spec.ts
@@ -10,4 +10,20 @@ test.describe('Assembly page', () => {
   test('if page is visible', async ({ page }) => {
     await expect(page.locator('h1')).toContainText('Team assembly');
   });
+
+  test('We can select encounter', async ({ page }) => {
+    const selectClub = page.locator('badman-select-club input');
+
+    await selectClub.fill('BC Broodrooster');
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('Enter');
+
+    const selectTeam = page.locator('badman-select-team');
+    await selectTeam.click();
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('Enter');
+
+
+    await expect(page.locator('badman-assembly')).toBeVisible();
+  });
 });
