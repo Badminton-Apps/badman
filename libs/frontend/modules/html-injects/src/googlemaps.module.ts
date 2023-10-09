60ce3bb48d5eed8143f12ef1b7c804a1922e2ace
Glenn Latomme <glenn.latomme@gmail.com>
2023-10-09 14:12:11 +0200

    feat: injecting google maps via js-api-loader

--- libs/frontend/modules/html-injects/src/googlemaps.module.ts
+++ libs/frontend/modules/html-injects/src/googlemaps.module.ts
@@ -1,10 +1,11 @@
 import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 import { Inject, NgModule, PLATFORM_ID } from '@angular/core';
 import { GOOGLE_MAPS_API_CONFIG } from '@ng-maps/google';
+import { Libraries, Loader } from '@googlemaps/js-api-loader';
 
 export type GooglMapsConfiguration = Readonly<{
   apiKey: string;
-  libraries?: string[];
+  libraries?: Libraries;
 }>;
 
 @NgModule({})
@@ -15,21 +16,36 @@ export class GoogleMapsModule {
     @Inject(DOCUMENT)
     d: Document,
     @Inject(GOOGLE_MAPS_API_CONFIG)
-    { apiKey, libraries }: GooglMapsConfiguration
+    { apiKey, libraries }: GooglMapsConfiguration,
   ) {
     if (isPlatformBrowser(platformId)) {
-      const maps = d.createElement('script');
-      maps.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`;
-      if (libraries) {
-        maps.src += `&libraries=${libraries.join(',')}`;
-      }
-      maps.async = true;
-      maps.crossOrigin = 'anonymous';
-      maps.defer = true;
+      const loader = new Loader({
+        apiKey,
+        version: 'weekly',
+        libraries: libraries ?? ([] as Libraries),
+      });
+      // Promise
+      loader
+        .load()
+        .then(() => {
+          console.log('injected google maps');
+        })
+        .catch((e) => {
+          console.error(e);
+        });
 
-      d.body.appendChild(maps);
+      // const maps = d.createElement('script');
+      // maps.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`;
+      // if (libraries) {
+      //   maps.src += `&libraries=${libraries.join(',')}`;
+      // }
+      // maps.async = true;
+      // maps.crossOrigin = 'anonymous';
+      // maps.defer = true;
 
-      console.log('injecting google maps');
+      // d.body.appendChild(maps);
+
+      // console.log('injecting google maps');
     }
   }
 }
