8a68dbecc5f15bf8627d8e3a864bb9378e87071f
cskiwi <glenn.latomme@gmail.com>
2023-10-16 14:16:10 +0200

    fix: return once done

--- apps/worker/sync/src/app/utils/correctWrongTeams.ts
+++ apps/worker/sync/src/app/utils/correctWrongTeams.ts
@@ -1,18 +1,18 @@
 const replacers = [
-  ['W&amp;L', 'W&L BV'],
-  ['W&L', 'W&L BV'],
-  ['Haneveld', 'Haeneveld'],
-  ['Gitse1D', 'Gitse 1D'],
-  ['DZ 99', 'DZ99'],
-  ['Nivelles', 'BC Nivellois'],
-  ['Fz Forza Webacsa', 'Webacsa'],
-  ['De Voskes BC', 'BC De Voskes'],
-  ['RBC VERVIERS', 'ROYAL BADMINTON CLUB VERVIERS'],
-  ['BC GRACE', 'BC Grâce 1'],
-  ['De Koekjes', 'Test 2G'],
-  ['Apparatuur', 'Test 1G'],
-  ['BCCM ALLEGRO', 'BC Cardinal Mercier']
-];
+  ['W&amp;L', 'W&L BV', true],
+  ['W&L', 'W&L BV', true],
+  ['Haneveld', 'Haeneveld', true],
+  ['Gitse1D', 'Gitse 1D', true],
+  ['DZ 99', 'DZ99', true],
+  ['Nivelles', 'BC Nivellois', true],
+  ['Fz Forza Webacsa', 'Webacsa', true],
+  ['De Voskes BC', 'BC De Voskes', true],
+  ['RBC VERVIERS', 'ROYAL BADMINTON CLUB VERVIERS', true],
+  ['BC GRACE', 'BC Grâce 1', true],
+  ['De Koekjes', 'Test 2G', true],
+  ['Apparatuur', 'Test 1G', true],
+  ['BCCM ALLEGRO', 'BC Cardinal Mercier', true]
+] as const;
 
 export const correctWrongTeams = (team: { 
   id?: string;
@@ -24,9 +24,13 @@ export const correctWrongTeams = (team: {
   team.name = team.name?.replace(/( +\(\d+\)?)/gi, '');
   team.name = team.name?.trim();
 
-  for (const [from, to] of replacers) {
+  for (const [from, to, finish] of replacers) {
     if (team.name.indexOf(from) > -1) {
       team.name = team.name.replace(from, to);
+
+      if (finish) {
+        break; 
+      }
     }
   }
 
