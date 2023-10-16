94ca232f2bb6e26d76b011b5437d6e96f41b2b19
cskiwi <glenn.latomme@gmail.com>
2023-10-16 11:41:27 +0200

    feat: adding dowgnrade in the database

---
+++ database/migrations/20231016090503-adding number of games used for downgrade.js
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, sequelize) => {
+    return queryInterface.sequelize.transaction(async (t) => {
+      try {
+        await queryInterface.addColumn(
+          { tableName: 'RankingSystems', schema: 'ranking' },
+          'minNumberOfGamesUsedForDowngrade',
+          {
+            type: sequelize.DataTypes.INTEGER,
+            allowNull: true,
+          },
+          { transaction: t },
+        );
+      } catch (err) {
+        console.error('We errored with', err?.message ?? err);
+        t.rollback();
+      }
+    });
+  },
+
+  down: async (queryInterface) => {
+    return queryInterface.sequelize.transaction(async (t) => {
+      try {
+        await queryInterface.removeColumn(
+          { tableName: 'RankingSystems', schema: 'ranking' },
+          'minNumberOfGamesUsedForDowngrade',
+          { transaction: t },
+        );
+      } catch (err) {
+        console.error('We errored with', err);
+        t.rollback();
+      }
+    });
+  },
+};
