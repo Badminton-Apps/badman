4f79c69df3b075eef4440b3480a9e208f92beadd
Glenn Latomme <glenn.latomme@gmail.com>
2023-10-17 09:22:22 +0200

    feat: switching cron to nestjs' cron feature

--- apps/worker/sync/src/app/crons/cron.ts
+++ apps/worker/sync/src/app/crons/cron.ts
@@ -1,47 +1,51 @@
 import { Sync, SyncQueue } from '@badman/backend-queue';
 import { InjectQueue } from '@nestjs/bull';
 import { Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
 import { Queue } from 'bull';
-import { parseExpression } from 'cron-parser';
-import moment from 'moment';
 
 @Injectable()
 export class CronService {
   private readonly logger = new Logger(CronService.name);
 
-  constructor(@InjectQueue(SyncQueue) syncQ: Queue) {
-    // remove all scheduled jobs from queue
-    this.logger.log('Clearing sync queue');
-    syncQ.empty().then(() => {
-      // Schedule new jobs
-      this._scheduleJob(syncQ, Sync.SyncEvents, '15 */4 * * *');
-      this._scheduleJob(syncQ, Sync.SyncRanking, '0 18 * * *');
-      this._scheduleJob(syncQ, Sync.CheckEncounters, '30 */4 * * *');
+  constructor(@InjectQueue(SyncQueue) private readonly syncQ: Queue) {}
+
+  @Cron('15 */4 * * *', {
+    name: Sync.SyncEvents,
+    timeZone: 'Europe/Paris',
+  })
+  public QueueingSyncEvents() {
+    this.logger.verbose('Queueing SyncEvents');
+
+    this.syncQ.add(Sync.SyncEvents, null, {
+      removeOnFail: 1,
+      removeOnComplete: true,
     });
   }
 
-  private _scheduleJob(
-    queue: Queue,
-    jobName: Sync,
-    cron: string,
-    data: unknown = null
-  ) {
-    // Getting next interval
-    const interval = parseExpression(cron);
-    const next = interval.next().getTime() - Date.now();
-    const readable = moment(next).format('HH:mm:ss');
+  @Cron('0 18 * * *', {
+    name: Sync.SyncRanking,
+    timeZone: 'Europe/Paris',
+  })
+  public QueueingSyncRanking() {
+    this.logger.verbose('Queueing SyncRanking');
 
-    // Scheduling job
-    this.logger.verbose(`Scheduling ${jobName} in ${readable}`);
-    return queue.add(jobName, data, {
+    this.syncQ.add(Sync.SyncRanking, null, {
       removeOnFail: 1,
       removeOnComplete: true,
-      delay: next,
-      repeat: {
-        cron: cron,
-        tz: 'Europe/Brussels',
-      },
-      jobId: jobName,
+    });
+  }
+
+  @Cron('30 */4 * * *', {
+    name: Sync.CheckEncounters,
+    timeZone: 'Europe/Paris',
+  })
+  public QueueingCheckEncounters() {
+    this.logger.verbose('Queueing CheckEncounters');
+
+    this.syncQ.add(Sync.CheckEncounters, null, {
+      removeOnFail: 1,
+      removeOnComplete: true,
     });
   }
 }
