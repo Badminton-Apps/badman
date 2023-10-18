797e841a3b86cd3154871b7d0506f58b3f55bf47
cskiwi <glenn.latomme@gmail.com>
2023-10-18 16:23:45 +0200

    fix: update player ranking via save button

--- libs/frontend/pages/player/src/pages/edit/tabs/edit-ranking/edit-ranking.component.ts
+++ libs/frontend/pages/player/src/pages/edit/tabs/edit-ranking/edit-ranking.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, Inject, Input, OnInit } from '@angular/core';
 import {
   FormControl,
@@ -6,16 +7,16 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { Apollo, gql } from 'apollo-angular';
-import { Player, RankingPlace, RankingSystem } from '@badman/frontend-models';
-import { BehaviorSubject, combineLatest, debounceTime, of } from 'rxjs';
-import { map, mergeMap } from 'rxjs/operators';
-import { APOLLO_CACHE } from '@badman/frontend-graphql';
-import { InMemoryCache } from '@apollo/client/core';
-import { CommonModule } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { InMemoryCache } from '@apollo/client/core';
+import { APOLLO_CACHE } from '@badman/frontend-graphql';
+import { Player, RankingPlace, RankingSystem } from '@badman/frontend-models';
+import { TranslateModule } from '@ngx-translate/core';
+import { Apollo, gql } from 'apollo-angular';
+import { BehaviorSubject, combineLatest, of } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'badman-edit-ranking',
@@ -29,6 +30,7 @@ import { MatInputModule } from '@angular/material/input';
     TranslateModule,
     FormsModule,
     MatInputModule,
+    MatButtonModule,
   ],
 })
 export class EditRankingComponent implements OnInit {
@@ -40,10 +42,13 @@ export class EditRankingComponent implements OnInit {
   @Input()
   player!: Player;
 
+  system?: RankingSystem;
+  rankingPlace?: RankingPlace;
+
   constructor(
     @Inject(APOLLO_CACHE) private cache: InMemoryCache,
     private appollo: Apollo,
-    private _snackBar: MatSnackBar
+    private _snackBar: MatSnackBar,
   ) {}
 
   ngOnInit() {
@@ -72,7 +77,7 @@ export class EditRankingComponent implements OnInit {
       map(([x]) =>
         x.data?.rankingSystems?.length > 0
           ? new RankingSystem(x.data.rankingSystems[0])
-          : null
+          : null,
       ),
       mergeMap((system) =>
         combineLatest([
@@ -100,112 +105,117 @@ export class EditRankingComponent implements OnInit {
               system: system?.id,
             },
           }),
-        ])
+        ]),
       ),
       map(([system, rankings]) => {
         return {
           system,
           player: new Player(rankings?.data?.player),
         };
-      })
+      }),
     );
 
     getRanings$.subscribe(({ system, player }) => {
       const singleControl = new FormControl(
         player?.lastRanking?.single ?? system?.amountOfLevels ?? 0,
-        [Validators.required]
+        [Validators.required],
       );
       const doubleControl = new FormControl(
         player?.lastRanking?.double ?? system?.amountOfLevels ?? 0,
-        [Validators.required]
+        [Validators.required],
       );
       const mixControl = new FormControl(
         player?.lastRanking?.mix ?? system?.amountOfLevels ?? 0,
-        [Validators.required]
+        [Validators.required],
       );
+
+      if (!system) {
+        throw new Error('System is not set');
+      }
+
+      this.system = system;
+      this.rankingPlace = player?.lastRanking;
 
       this.rankingForm = new FormGroup({
         single: singleControl,
         double: doubleControl,
         mix: mixControl,
       });
-
-      this.rankingForm.valueChanges
-        .pipe(debounceTime(600))
-        .subscribe((value) => {
-          if (this.rankingForm.valid) {
-            if (!this.player) {
-              throw new Error('Player is not set');
-            }
-            if (!this.player.id) {
-              throw new Error('Player id is not set');
-            }
-
-            let mutation;
-
-            if (player?.lastRanking?.id) {
-              mutation = {
-                mutation: gql`
-                  mutation UpdateRanking($data: RankingPlaceUpdateInput!) {
-                    updateRankingPlace(data: $data) {
-                      id
-                    }
-                  }
-                `,
-                variables: {
-                  data: {
-                    id: player?.lastRanking?.id,
-                    single: +value.single,
-                    double: +value.double,
-                    mix: +value.mix,
-                  },
-                },
-              };
-            } else {
-              mutation = {
-                mutation: gql`
-                  mutation NewRanking($data: RankingPlaceNewInput!) {
-                    newRankingPlace(data: $data) {
-                      id
-                    }
-                  }
-                `,
-                variables: {
-                  data: {
-                    systemId: system?.id,
-                    playerId: this.player.id,
-                    single: +value.single,
-                    double: +value.double,
-                    mix: +value.mix,
-                    rankingDate: system?.caluclationIntervalLastUpdate,
-                    updatePossible: true,
-                  },
-                },
-              };
-            }
-
-            this.appollo
-              .mutate<{
-                updateRankingPlace?: Player;
-                newRankingPlace?: Player;
-              }>(mutation)
-              .subscribe((result) => {
-                this.invalidatePlayerRanking(
-                  new Player(
-                    result?.data?.updateRankingPlace ??
-                      result?.data?.newRankingPlace
-                  )
-                );
-                this.update$.next(null);
-
-                this._snackBar.open('Saved', undefined, {
-                  duration: 1000,
-                  panelClass: 'success',
-                });
-              });
-          }
-        });
     });
+  }
+
+  saveRanking() {
+    if (this.rankingForm.valid) {
+      if (!this.player) {
+        throw new Error('Player is not set');
+      }
+      if (!this.player.id) {
+        throw new Error('Player id is not set');
+      }
+
+      let mutation;
+      const value = this.rankingForm.value;
+
+      if (this.rankingPlace?.id) {
+        mutation = {
+          mutation: gql`
+            mutation UpdateRanking($data: RankingPlaceUpdateInput!) {
+              updateRankingPlace(data: $data) {
+                id
+              }
+            }
+          `,
+          variables: {
+            data: {
+              id: this.rankingPlace?.id,
+              single: +value.single,
+              double: +value.double,
+              mix: +value.mix,
+            },
+          },
+        };
+      } else {
+        mutation = {
+          mutation: gql`
+            mutation NewRanking($data: RankingPlaceNewInput!) {
+              newRankingPlace(data: $data) {
+                id
+              }
+            }
+          `,
+          variables: {
+            data: {
+              systemId: this.system?.id,
+              playerId: this.player.id,
+              single: +value.single,
+              double: +value.double,
+              mix: +value.mix,
+              rankingDate: this.system?.caluclationIntervalLastUpdate,
+              updatePossible: true,
+            },
+          },
+        };
+      }
+
+      this.appollo
+        .mutate<{
+          updateRankingPlace?: Player;
+          newRankingPlace?: Player;
+        }>(mutation)
+        .subscribe((result) => {
+          this.invalidatePlayerRanking(
+            new Player(
+              result?.data?.updateRankingPlace ?? result?.data?.newRankingPlace,
+            ),
+          );
+          this.update$.next(null);
+
+          this._snackBar.open('Saved', undefined, {
+            duration: 1000,
+            panelClass: 'success',
+          });
+        });
+    }
   }
 
   invalidatePlayerRanking(player: Player) {
