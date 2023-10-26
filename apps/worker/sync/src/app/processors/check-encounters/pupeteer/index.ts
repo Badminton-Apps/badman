7d014e2f8655ab0e2e87113774488deb32cfa14b
Glenn Latomme <glenn.latomme@gmail.com>
2023-10-26 13:34:20 +0200

    feat: adding detail info to page

--- apps/worker/sync/src/app/processors/check-encounters/pupeteer/index.ts
+++ apps/worker/sync/src/app/processors/check-encounters/pupeteer/index.ts
@@ -3,5 +3,6 @@ export * from './gotoEncounterPage';
 export * from './getDetailAccepted';
 export * from './getDetailComment';
 export * from './getDetailEntered';
+export * from './getDetailInfo';
 export * from './getTime';
 // end:ng42.barrel
