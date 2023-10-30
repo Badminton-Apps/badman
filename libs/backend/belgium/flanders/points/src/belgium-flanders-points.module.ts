d33c4fe442843690406bbe1fea1a567f4bdc432a
cskiwi <glenn.latomme@gmail.com>
2023-10-30 18:37:09 +0100

    ci: dependen packages

--- libs/backend/belgium/flanders/points/src/belgium-flanders-points.module.ts
+++ libs/backend/belgium/flanders/points/src/belgium-flanders-points.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BelgiumFlandersPointsService } from './services/belgium-flanders-points.service';
+import { DatabaseModule } from '@badman/backend-database';
 
 @Module({
   controllers: [],
+  imports: [DatabaseModule],
   providers: [BelgiumFlandersPointsService],
   exports: [BelgiumFlandersPointsService],
 })
