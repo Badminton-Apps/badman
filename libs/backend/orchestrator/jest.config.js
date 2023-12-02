bb739251e727a09229ea9e9671c16d377f0216a0
cskiwi <glenn.latomme@gmail.com>
2023-12-02 15:53:53 +0100

    got it builded

---
+++ libs/backend/orchestrator/jest.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+module.exports = {
+  displayName: 'notifications',
+  preset: '../../../jest.preset.js',
+  globals: {},
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/libs/backend/notifications',
+};
