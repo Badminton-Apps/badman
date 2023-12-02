0854b597f952c089b976857f7b61427d1b3babd6
cskiwi <glenn.latomme@gmail.com>
2023-12-02 16:09:23 +0100

    jest.ts get back over here!

--- libs/backend/notifications/jest.config.ts
+++ libs/backend/notifications/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
-module.exports = {
-  displayName: 'notifications',
+export default {
+  displayName: 'health',
   preset: '../../../jest.preset.js',
   globals: {},
   testEnvironment: 'node',
@@ -13,5 +13,5 @@ module.exports = {
     ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/libs/backend/notifications',
+  coverageDirectory: '../../../coverage/libs/backend/health',
 };
