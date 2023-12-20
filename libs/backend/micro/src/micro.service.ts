3ff92beaace07f31c3b11554ac1e6e259849ef25
cskiwi <glenn.latomme@gmail.com>
2023-12-20 23:21:23 +0100

    just do it like this for now

--- libs/backend/micro/src/micro.service.ts
+++ libs/backend/micro/src/micro.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import { RmqContext } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+import { RmqContext, RmqOptions, Transport } from '@nestjs/microservices';
 
 @Injectable()
 export class MicroService {
   // constructor(private readonly configService: ConfigService<ConfigType>) {}
-  // getOptions(queue: string, noAck = false): RmqOptions {
-  //   return {
-  //     transport: Transport.RMQ,
-  //     options: {
-  //       urls: [this.configService.get<string>('RABBIT_MQ_URI')!],
-  //       queue: this.configService.get<string>(`RABBIT_MQ_${queue}_QUEUE`),
-  //       noAck,
-  //       persistent: true,
-  //     },
-  //   };
-  // }
+  constructor(private readonly configService: ConfigService) {}
+
+  getOptions(queue: string, noAck = false): RmqOptions {
+    return {
+      transport: Transport.RMQ,
+      options: {
+        urls: [this.configService.get<string>('RABBIT_MQ_URI')!],
+        queue: this.configService.get<string>(`RABBIT_MQ_${queue}_QUEUE`),
+        noAck,
+        persistent: true,
+      },
+    };
+  }
 
   ack(context: RmqContext) {
     const channel = context.getChannelRef();
