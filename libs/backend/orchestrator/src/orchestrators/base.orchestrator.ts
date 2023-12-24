5fb0eb3fc597805bc1fb97479a58cfb7a520e992
cskiwi <glenn.latomme@gmail.com>
2023-12-24 14:11:24 +0100

    fix: remove timeout

--- libs/backend/orchestrator/src/orchestrators/base.orchestrator.ts
+++ libs/backend/orchestrator/src/orchestrators/base.orchestrator.ts
@@ -1,20 +1,18 @@
 import { Service } from '@badman/backend-database';
 import { EventsGateway } from '@badman/backend-websockets';
-import { EVENTS } from '@badman/utils';
-import { OnGlobalQueueWaiting } from '@nestjs/bull';
+import { ConfigType, EVENTS } from '@badman/utils';
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Cron } from '@nestjs/schedule';
 import { Queue } from 'bull';
 import { Services } from '../services';
 import { RenderService } from '../services/render.service';
-import { ConfigService } from '@nestjs/config';
-import { ConfigType } from '@badman/utils';
 
 export class OrchestratorBase {
   protected logger = new Logger(OrchestratorBase.name);
   private hasStarted = false;
-  private timeout?: NodeJS.Timeout;
-  private timeoutTime = 1000 * 60 * 5; // 5 minutes
+  private timeoutTime = 1000 * 60 * 5 ; // 5 minutes
+  private startTime = new Date();
 
   constructor(
     protected readonly serviceName: Services,
@@ -61,13 +59,6 @@ export class OrchestratorBase {
     });
   }
 
-  @OnGlobalQueueWaiting()
-  async queueWaiting() {
-    if (!this.hasStarted) {
-      await this._checkAndStartStopIfNeeded();
-    }
-  }
-
   private async _getService() {
     let service = await Service.findOne({ where: { name: this.serviceName } });
     // create service if it doesn't exist
@@ -85,24 +76,30 @@ export class OrchestratorBase {
     const counts = await this.queue.getJobCounts();
 
     if (counts.waiting > 0 || counts.active > 0) {
-      // clear timeout if it exists because we have jobs in queue
-      clearTimeout(this.timeout);
+      // reset start time
+      this.startTime = new Date();
+
       if (!this.hasStarted) {
         this.logger.debug(
-          `[${this.serviceName}] Found ${counts.waiting} jobs in queue, starting worker`,
+          `[${this.serviceName}] Found ${counts.waiting} waiting and ${counts.active} jobs in queue, starting worker`,
         );
         this.startServer();
         this.hasStarted = true;
       }
     } else {
       if (this.hasStarted) {
-        this.timeout = setTimeout(() => {
-          this.logger.debug(
-            `[${this.serviceName}] No more jobs in queue, stopping worker`,
-          );
-          this.stopServer();
-          this.hasStarted = false;
-        }, this.timeoutTime);
+        const now = new Date();
+        const diff = now.getTime() - this.startTime.getTime();
+
+        if (diff < this.timeoutTime) {
+          return;
+        }
+
+        this.logger.debug(
+          `[${this.serviceName}] No more jobs in queue, stopping worker`,
+        );
+        this.stopServer();
+        this.hasStarted = false;
       }
     }
   }
