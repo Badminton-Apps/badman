8d32dab44109571c39e043fd1a5b0f46b1c8f40a
cskiwi <glenn.latomme@gmail.com>
2024-01-29 16:16:05 +0100

    fix: debuging

--- apps/scripts/webpack.config.js
+++ apps/scripts/webpack.config.js
@@ -4,5 +4,8 @@ const { composePlugins, withNx } = require("@nx/webpack");
 module.exports = composePlugins(withNx(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
+  config.devtool = 'source-map'
+
+  
   return config;
 });
--- apps/worker/ranking/webpack.config.js
+++ apps/worker/ranking/webpack.config.js
@@ -4,5 +4,7 @@ const { composePlugins, withNx } = require("@nx/webpack");
 module.exports = composePlugins(withNx(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
+  config.devtool = 'source-map'
+
   return config;
 });
