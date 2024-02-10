8bead6cd0af105fc6a00874354e3fd73810dc817
cskiwi <glenn.latomme@gmail.com>
2024-02-10 11:00:47 +0100

    fix: updated the packages

--- libs/backend/pupeteer/src/shared.ts
+++ libs/backend/pupeteer/src/shared.ts
@@ -2,7 +2,7 @@ import { ElementHandle, launch, Page } from 'puppeteer';
 
 export async function getBrowser(headless = true) {
   return await launch({
-    headless: headless ? 'new' : false,
+    headless: headless ? true : false,
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
