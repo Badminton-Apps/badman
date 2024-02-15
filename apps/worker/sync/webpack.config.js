01bdad44798086bcadcb70a47b3b08518f30fb42
cskiwi <glenn.latomme@gmail.com>
2024-02-15 14:59:35 +0100

    ci: always good idea to modify the right file

--- apps/worker/sync/webpack.config.js
+++ apps/worker/sync/webpack.config.js
@@ -11,7 +11,19 @@ module.exports = {
       compiler: 'tsc',
       main: './src/main.ts',
       tsConfig: './tsconfig.app.json',
-      assets: ['./src/assets'],
+      assets: [
+        './src/assets',
+        {
+          glob: '**/*',
+          input: 'libs/backend/mailing/src/compile',
+          output: 'compile/libs/mailing',
+        },
+        {
+          glob: '**/*',
+          input: 'libs/backend/translate/assets',
+          output: 'assets',
+        },
+      ],
       optimization: false,
       outputHashing: 'none',
     }),
