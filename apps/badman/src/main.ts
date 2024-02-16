516e745e213367538621561c6d5288294ce74103
cskiwi <glenn.latomme@gmail.com>
2024-02-16 13:21:19 +0100

    feat: view transitions

--- apps/badman/src/main.ts
+++ apps/badman/src/main.ts
@@ -1,7 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
 import { provideAnimations } from '@angular/platform-browser/animations';
-
+import { AppModule } from './app/app.module';
 
 function bootstrap() {
   platformBrowserDynamic()
