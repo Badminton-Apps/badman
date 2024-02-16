5039fd6fa384dc7debe074a51cbc9829390aa2d0
cskiwi <glenn.latomme@gmail.com>
2024-02-16 15:32:35 +0100

    ci: some changes

--- apps/badman/src/app/app.component.ts
+++ apps/badman/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { otherAnimation } from './app.animations';
 
 @Component({
   selector: 'badman-root',
   template: `<badman-shell>
     <router-outlet></router-outlet>
   </badman-shell>`,
-  animations: [otherAnimation],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {}
