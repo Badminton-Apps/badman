3ea11f0ee883a8905106912203397fd4de76f7c0
cskiwi <glenn.latomme@gmail.com>
2024-02-17 23:31:38 +0100

    test: running more test in one file to improve performance

--- apps/badman-e2e/src/tests/landing.e2e.spec.ts
+++ apps/badman-e2e/src/tests/landing.e2e.spec.ts
@@ -1,16 +1,13 @@
 import { expect } from '@playwright/test';
 import { bTest } from '../fixture';
 
-bTest.describe('Landing page', () => {
-  bTest('if page is visible', async ({ homePage }) => {
-    await expect(homePage.ranking).toBeVisible();
-  });
+bTest('Landing page', async ({ homePage }) => {
+  // if page is visible
+  await expect(homePage.ranking).toBeVisible();
 
-  bTest('if header is visible', async ({ homePage }) => {
-    await expect(homePage.ranking.locator('h3')).toContainText('Ranking table');
-  });
+  // if header is visible
+  await expect(homePage.ranking.locator('h3')).toContainText('Ranking table');
 
-  bTest('should contain 12 rows', async ({ homePage }) => {
-    await expect(homePage.ranking.locator('tbody tr')).toHaveCount(12);
-  });
+  // should contain 12 rows
+  await expect(homePage.ranking.locator('tbody tr')).toHaveCount(12);
 });
