d20cd9a64ddf8fc4c817c5855d3ff5d465768fb0
cskiwi <glenn.latomme@gmail.com>
2024-02-17 23:31:38 +0100

    test: running more test in one file to improve performance

--- apps/badman-e2e/src/tests/assembly.e2e.spec.ts
+++ apps/badman-e2e/src/tests/assembly.e2e.spec.ts
@@ -15,52 +15,44 @@ bTest.describe('AssemblyPage page', () => {
     bTest('Select encounter', async ({ assemblyPage }) => {
       await expect(assemblyPage.encounterSelect).toContainText('against BC Tandpasta 1G');
       await expect(assemblyPage.playerList).toBeVisible();
-    });
 
-    bTest('Player 8888 should be visible', async ({ assemblyPage }) => {
+      // Player 8888 should be visible
       const player = await assemblyPage.getPlayer('M 8-8-8 BC Broodrooster');
 
       await expect(player).toBeVisible();
     });
 
-    bTest.describe('Drag and drop', () => {
-      bTest('Drag player 8888 to single men', async ({ assemblyPage }) => {
-        await assemblyPage.dragPlayer('M 8-8-8 BC Broodrooster', assemblyPage.single1List);
+    bTest('Drag and drop', async ({ assemblyPage }) => {
+      // Drag player 8888 to single men
+      await assemblyPage.dragPlayer('M 8-8-8 BC Broodrooster', assemblyPage.single1List);
 
-        await expect(assemblyPage.single1List).toContainText('M 8-8-8 BC Broodrooster');
+      await expect(assemblyPage.single1List).toContainText('M 8-8-8 BC Broodrooster');
 
-        await expect(assemblyPage.titulars.index).toContainText('Index: 132');
-      });
+      await expect(assemblyPage.titulars.index).toContainText('Index: 132');
 
-      bTest('Drag player 8888 to single women', async ({ assemblyPage }) => {
-        await assemblyPage.dragPlayer('M 8-8-8 BC Broodrooster', assemblyPage.single1List);
+      // Drag player 8888 to single women
+      await assemblyPage.dragPlayer('M 8-8-8 BC Broodrooster', assemblyPage.single1List);
 
-        await expect(assemblyPage.single3List).not.toContainText('M 8-8-8 BC Broodrooster');
-      });
+      await expect(assemblyPage.single3List).not.toContainText('M 8-8-8 BC Broodrooster');
 
-      bTest('Drag player 8888 and 999 to doubles', async ({ assemblyPage }) => {
-        await assemblyPage.dragPlayer('M 8-8-8 BC Broodrooster', assemblyPage.double1List);
-        await assemblyPage.dragPlayer('M 9-9-9 BC Broodrooster', assemblyPage.double1List);
+      // Drag player 8888 and 999 to doubles
+      await assemblyPage.dragPlayer('M 8-8-8 BC Broodrooster', assemblyPage.double1List);
+      await assemblyPage.dragPlayer('M 9-9-9 BC Broodrooster', assemblyPage.double1List);
 
-        await expect(assemblyPage.double1List).toContainText('M 8-8-8 BC Broodrooster');
-        await expect(assemblyPage.double1List).toContainText('M 9-9-9 BC Broodrooster');
+      await expect(assemblyPage.double1List).toContainText('M 8-8-8 BC Broodrooster');
+      await expect(assemblyPage.double1List).toContainText('M 9-9-9 BC Broodrooster');
 
-        await expect(assemblyPage.titulars.index).toContainText('Index: 123');
-      });
+      await expect(assemblyPage.titulars.index).toContainText('Index: 123');
 
-      bTest(
-        'Drag player 8888 and 999 to singles reversed and check validation',
-        async ({ assemblyPage }) => {
-          await assemblyPage.dragPlayer('M 8-8-8 BC Broodrooster', assemblyPage.single2List);
-          await assemblyPage.dragPlayer('M 9-9-9 BC Broodrooster', assemblyPage.single1List);
+      // Drag player 8888 and 999 to singles reversed and check validation
+      await assemblyPage.dragPlayer('M 8-8-8 BC Broodrooster', assemblyPage.single2List);
+      await assemblyPage.dragPlayer('M 9-9-9 BC Broodrooster', assemblyPage.single1List);
 
-          await expect(assemblyPage.single1List).toContainText('M 9-9-9 BC Broodrooster');
-          await expect(assemblyPage.single2List).toContainText('M 8-8-8 BC Broodrooster');
+      await expect(assemblyPage.single1List).toContainText('M 9-9-9 BC Broodrooster');
+      await expect(assemblyPage.single2List).toContainText('M 8-8-8 BC Broodrooster');
 
-          await expect(assemblyPage.validationOverview).toContainText(
-            'M 8-8-8 BC Broodrooster in single 2 has a higher index than M 9-9-9 BC Broodrooster in single 1.',
-          );
-        },
+      await expect(assemblyPage.validationOverview).toContainText(
+        'M 8-8-8 BC Broodrooster in single 2 has a higher index than M 9-9-9 BC Broodrooster in single 1.',
       );
     });
   });
