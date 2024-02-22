d5cda614ea475d83955c0c6bf3168dfc037f1e23
cskiwi <glenn.latomme@gmail.com>
2024-02-22 15:40:24 +0100

    fix: sort games that singles are always last

--- libs/utils/src/lib/sorts/games.sort.ts
+++ libs/utils/src/lib/sorts/games.sort.ts
@@ -1,9 +1,13 @@
+import { GameType } from '../enums';
+
 export const sortGames = (
   a: Partial<{
     playedAt: Date;
+    gameType: GameType;
   }>,
   b: Partial<{
     playedAt: Date;
+    gameType: GameType;
   }>,
   order: 'asc' | 'desc' = 'desc',
 ) => {
@@ -20,6 +24,15 @@ export const sortGames = (
   } else if (a.playedAt > b.playedAt) {
     return order === 'asc' ? 1 : -1;
   } else {
+    // GameType.S is always the last one
+    if (a.gameType === GameType.S && b.gameType !== GameType.S) {
+      return 1;
+    }
+
+    if (a.gameType !== GameType.S && b.gameType === GameType.S) {
+      return -1;
+    }
+
     return 0;
   }
 };
