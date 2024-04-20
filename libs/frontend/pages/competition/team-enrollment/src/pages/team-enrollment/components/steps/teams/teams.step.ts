0b2f0bb45a6eb63f00471d086d18f9c8034e396e
cskiwi <glenn.latomme@gmail.com>
2024-04-20 16:20:16 +0200

    feat: loading data when selecting club

--- libs/frontend/pages/competition/team-enrollment/src/pages/team-enrollment/components/steps/teams/teams.step.ts
+++ libs/frontend/pages/competition/team-enrollment/src/pages/team-enrollment/components/steps/teams/teams.step.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
+  Signal,
   computed,
   effect,
   inject,
@@ -17,13 +18,15 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { SubEventTypeEnum } from '@badman/utils';
+import { SubEventType, SubEventTypeEnum, getUpcommingSeason } from '@badman/utils';
 import { TranslateModule } from '@ngx-translate/core';
 import { injectDestroy } from 'ngxtension/inject-destroy';
 import { TEAMS } from '../../../../../forms';
 import { TeamEnrollmentDataService } from '../../../service/team-enrollment.service';
 import { TeamForm } from '../teams-transfer';
 import { TeamEnrollmentComponent } from './components';
+import { RankingSystemService } from '@badman/frontend-graphql';
+import { RankingSystem, SubEventCompetition } from '@badman/frontend-models';
 
 @Component({
   selector: 'badman-teams-step',
@@ -48,9 +51,12 @@ import { TeamEnrollmentComponent } from './components';
 export class TeamsStepComponent {
   private readonly destroy$ = injectDestroy();
   private readonly dataService = inject(TeamEnrollmentDataService);
+  private readonly systemService = inject(RankingSystemService);
 
   club = this.dataService.state.club;
   loaded = this.dataService.state.loadedTeams;
+  season = this.dataService.state.season as Signal<number>;
+  system = this.systemService.system as Signal<RankingSystem>;
 
   formGroup = input.required<FormGroup>();
   teams = computed(
@@ -61,6 +67,7 @@ export class TeamsStepComponent {
   );
 
   eventsPerType = this.dataService.state.eventsPerType;
+  eventTypes = Object.values(SubEventTypeEnum);
 
   constructor() {
     // set initial controls
@@ -75,8 +82,35 @@ export class TeamsStepComponent {
 
       // use the state but don't update effect when it changes
       untracked(() => {
-        console.log('TeamsStepComponent: loading teams', this.eventsPerType());
+        for (const type of this.eventTypes) {
+          const subEvents = this.eventsPerType()[type];
+          const teams = this.teams().get(type) as FormArray<TeamForm>;
+
+          // iterate over each team and set the initial sub event
+          teams.controls.forEach((team) => {
+            this.setInitialSubEvent(team, subEvents);
+          });
+        }
       });
     });
+  }
+
+  getTypeArray(type: SubEventType) {
+    return this.teams()?.controls[type] as FormArray<TeamForm>;
+  }
+
+  addTeam(type: SubEventType) {
+    throw new Error('Not implemented');
+  }
+
+  setInitialSubEvent(team: TeamForm, subEvents: SubEventCompetition[]) {
+    const subEventId = subEvents[0]?.id;
+
+    if (!subEventId) {
+      console.error('No sub events found for team', team.value, subEvents);
+      return;
+    }
+
+    team.get('entry')?.get('subEventId')?.setValue(subEventId);
   }
 }
