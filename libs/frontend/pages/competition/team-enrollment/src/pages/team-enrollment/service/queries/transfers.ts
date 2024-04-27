0cc7c8dd01a087811eacbd0f661b36600a943c90
cskiwi <glenn.latomme@gmail.com>
2024-04-27 17:23:33 +0200

    feat: adding club filter

---
+++ libs/frontend/pages/competition/team-enrollment/src/pages/team-enrollment/service/queries/transfers.ts
@@ -0,0 +1,40 @@
+import { Club, Player } from '@badman/frontend-models';
+import { Apollo, gql } from 'apollo-angular';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export const loadTransersAndLoans = (apollo: Apollo, clubId?: string | null, season?: number) => {
+  if (!clubId) {
+    console.error('No clubId or season provided');
+    return of([]);
+  }
+
+  return apollo
+    .query<{ club: Partial<Club> }>({
+      query: gql`
+        query GetLoansAndTransfersForSeason${season}($id: ID!, $active: Boolean, $where: JSONObject) {
+          club(id: $id) {
+            id
+            players(active: $active, where: $where) {
+              id
+              fullName
+              gender
+            }
+          }
+        }
+      `,
+      variables: {
+        id: clubId,
+        active: false,
+        where: {
+          '$ClubPlayerMembership.start$': {
+            $gte: `${season}-03-01`,
+          },
+          '$ClubPlayerMembership.end$': null,
+        },
+      },
+    })
+    .pipe(
+      map((result) => (result?.data?.club?.players ?? [])?.map((player) => new Player(player))),
+    );
+};
