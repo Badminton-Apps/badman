14bf948b97385fef0615b4809832c5607b115dd8
cskiwi <glenn.latomme@gmail.com>
2024-05-04 14:54:36 +0200

    feat: upgraded ranking table

--- libs/frontend/components/src/ranking-table/ranking-table.component.ts
+++ libs/frontend/components/src/ranking-table/ranking-table.component.ts
@@ -1,112 +1,52 @@
 import { CommonModule } from '@angular/common';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Injector,
-  OnInit,
-  PLATFORM_ID,
-  Signal,
-  TransferState,
-  computed,
-  inject,
-  input,
-} from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { MatTableModule } from '@angular/material/table';
+import { ChangeDetectionStrategy, Component, effect, inject, input } from '@angular/core';
 import { RankingSystemService } from '@badman/frontend-graphql';
-import { RankingSystem } from '@badman/frontend-models';
-import { transferState } from '@badman/frontend-utils';
-import { TranslateModule } from '@ngx-translate/core';
-import { Apollo, gql } from 'apollo-angular';
-import { first, map } from 'rxjs';
+import { MtxGrid, MtxGridColumn } from '@ng-matero/extensions/grid';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { RankingTableService } from './ranking-table.service';
 
 @Component({
   selector: 'badman-ranking-table',
   standalone: true,
-  imports: [CommonModule, TranslateModule, MatTableModule],
+  imports: [CommonModule, TranslateModule, MtxGrid],
   templateUrl: './ranking-table.component.html',
   styleUrls: ['./ranking-table.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RankingTableComponent implements OnInit {
+export class RankingTableComponent {
   private readonly rankingSystemService = inject(RankingSystemService);
-  private readonly apollo = inject(Apollo);
-  private readonly transferState = inject(TransferState);
-  private readonly platformId = inject(PLATFORM_ID);
-  private readonly injector = inject(Injector);
-
+  private readonly rankingTableService = inject(RankingTableService);
+  private readonly translate = inject(TranslateService);
   id = input<string | null>(null);
 
-  displayedColumns = ['level', 'pointsToGoUp', 'pointsToGoDown', 'pointsWhenWinningAgainst'];
-
-  system = this.rankingSystemService.system as Signal<RankingSystem>;
-
-  ngOnInit() {
-    if (this.id() !== null) {
-      this.system = toSignal(this._loadRanking(this.id()), {
-        injector: this.injector,
-      }) as Signal<RankingSystem>;
-    }
-  }
-
-  dataSource = computed(() => {
-    if (!this.system()) {
-      return [];
-    }
-
-    let level = this.system().amountOfLevels ?? 0;
-    return this.system().pointsWhenWinningAgainst?.map((winning: number, index: number) => {
-      return {
-        level: level--,
-        pointsToGoUp: level !== 0 ? Math.round(this.system().pointsToGoUp?.[index] ?? 0) : null,
-        pointsToGoDown:
-          index === 0 ? null : Math.round(this.system().pointsToGoDown?.[index - 1] ?? 0),
-        pointsWhenWinningAgainst: Math.round(winning),
-      } as RankingScoreTable;
-    });
-  }) as () => RankingScoreTable[];
-
-  _loadRanking(systemId: string | null) {
+  columns: MtxGridColumn[] = [
+    { header: this.translate.stream('all.faq.points.table.level'), field: 'level' },
     {
-      return this.apollo
-        .query<{
-          rankingSystem: Partial<RankingSystem>;
-        }>({
-          query: gql`
-            query GetSystemForTable($id: ID) {
-              rankingSystem(id: $id) {
-                id
-                name
-                amountOfLevels
-                pointsToGoUp
-                pointsToGoDown
-                pointsWhenWinningAgainst
-                calculationLastUpdate
-                primary
-              }
-            }
-          `,
-          variables: {
-            id: systemId,
-          },
-        })
-        .pipe(
-          transferState('rankingKey-' + systemId, this.transferState, this.platformId),
-          map((result) => {
-            if (!result?.data.rankingSystem) {
-              throw new Error('No player');
-            }
-            return new RankingSystem(result.data.rankingSystem);
-          }),
-          first(),
-        );
-    }
+      header: this.translate.stream('all.faq.points.table.points-needed-up'),
+      field: 'pointsToGoUp',
+    },
+    {
+      header: this.translate.stream('all.faq.points.table.points-needed-down'),
+      field: 'pointsToGoDown',
+    },
+    {
+      header: this.translate.stream('all.faq.points.table.points-won'),
+      field: 'pointsWhenWinningAgainst',
+    },
+  ];
+
+  dataSource = this.rankingTableService.state.table;
+  loaded = this.rankingTableService.state.loaded;
+
+  constructor() {
+    effect(() => {
+      if (this.id()) {
+        this.rankingTableService.filter.patchValue({ systemId: this.id() });
+      } else {
+        this.rankingTableService.filter.patchValue({
+          systemId: this.rankingSystemService.systemId(),
+        });
+      }
+    });
   }
 }
-
-type RankingScoreTable = {
-  level: number;
-  pointsToGoUp: number;
-  pointsToGoDown: number;
-  pointsWhenWinningAgainst: number;
-};
