e5044547e83d5a3f40a566175a284bbbe90f33ee
cskiwi <glenn.latomme@gmail.com>
2024-05-05 17:20:56 +0200

    fix: started adding typescript support for querying models

--- libs/backend/database/src/models/system/cron-job.model.ts
+++ libs/backend/database/src/models/system/cron-job.model.ts
@@ -1,4 +1,4 @@
-import { QueueName, Simulation, Sync } from '@badman/backend-queue';
+import { QueueName, Ranking, Simulation, Sync, UpdateRankingJob } from '@badman/backend-queue';
 import { Field, ID, InputType, Int, ObjectType, OmitType, PartialType } from '@nestjs/graphql';
 import {
   Column,
@@ -11,30 +11,31 @@ import {
   Unique,
 } from 'sequelize-typescript';
 import { CronJobMetaType } from '../../types';
+import { CreationOptional, InferAttributes, InferCreationAttributes } from 'sequelize';
 
 @Table({
   timestamps: true,
   schema: 'system',
 })
 @ObjectType({ description: 'Cron job' })
-export class CronJob extends Model {
+export class CronJob extends Model<InferAttributes<CronJob>, InferCreationAttributes<CronJob>> {
   @Field(() => ID)
   @Default(DataType.UUIDV4)
   @IsUUID(4)
   @PrimaryKey
   @Column(DataType.UUIDV4)
-  override id!: string;
+  declare id: CreationOptional<string>;
 
   @Field(() => Date, { nullable: true })
-  override updatedAt?: Date;
+  declare updatedAt?: Date;
 
   @Field(() => Date, { nullable: true })
-  override createdAt?: Date;
+  declare createdAt?: Date;
 
   @Field(() => String, { nullable: false })
   @Unique('unique_constraint')
   @Column(DataType.STRING)
-  name!: string;
+  declare name: string;
 
   @Field(() => String, { nullable: false })
   @Column(DataType.STRING)
@@ -60,7 +61,7 @@ export class CronJob extends Model {
   @Column({
     type: DataType.VIRTUAL,
     get(this: CronJob) {
-      const amount = this.getDataValue('amount');
+      const amount = this.getDataValue('amount') ?? 0;
       return amount > 0;
     },
   })
@@ -87,7 +88,7 @@ export class CronJobNewInput extends PartialType(
 export type CronJobMeta = QueueCronJob;
 
 export interface QueueCronJob {
-  jobName: Sync | Simulation;
+  jobName: Sync | Simulation | Ranking;
   queueName: QueueName;
-  arguments: string;
+  arguments: UpdateRankingJob;
 }
