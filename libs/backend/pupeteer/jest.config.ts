0a7a0eef2a2b1d788d2d7fd97750d759750f2b1b
cskiwi <glenn.latomme@gmail.com>
2024-07-31 13:05:59 +0200

    ci: remove pass with no tests

--- libs/backend/pupeteer/jest.config.ts
+++ libs/backend/pupeteer/jest.config.ts
@@ -3,7 +3,6 @@ export default {
   // displayName: 'backend-pupeteer',
   preset: '../../../jest.preset.js',
   testEnvironment: 'node',
-  passWithNoTests: true,
   testTimeout: 1000 * 60 * 10,
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
--- libs/frontend/pages/transfers/jest.config.ts
+++ libs/frontend/pages/transfers/jest.config.ts
@@ -3,7 +3,6 @@ export default {
   // displayName: 'frontend-transfers',
   preset: '../../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  passWithNoTests: true,
   testTimeout: 1000 * 60 * 10,
   coverageDirectory: '../../../../coverage/libs/frontend/pages/transfer',
   transform: {
