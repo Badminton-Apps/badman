3e62345b48da8ef2dc537f548ef6347fb345ea45
cskiwi <glenn.latomme@gmail.com>
2024-08-04 15:59:43 +0200

    sonar linting

--- libs/frontend/pages/club/src/pages/detail/club-encounters/club-encounters.ts
+++ libs/frontend/pages/club/src/pages/detail/club-encounters/club-encounters.ts
@@ -1,18 +1,18 @@
 import { JsonPipe } from '@angular/common';
-import { Component, computed, effect, inject, input } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckbox } from '@angular/material/checkbox';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatRadioButton, MatRadioGroup } from '@angular/material/radio';
-import { Club, EncounterCompetition, Team } from '@badman/frontend-models';
+import { MatSlideToggleChange, MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { EncounterCompetition, Team } from '@badman/frontend-models';
 import { MtxGrid, MtxGridColumn } from '@ng-matero/extensions/grid';
 import { MtxSelect } from '@ng-matero/extensions/select';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
-import { ClubEncounterService } from './club-encounters.service';
 import moment from 'moment';
-import { MatSlideToggleChange, MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ClubEncounterService } from './club-encounters.service';
 
 @Component({
   selector: 'badman-club-encounters',
