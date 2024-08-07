b3f5cfbdc073e26ad160957dfa93eb4723f06125
cskiwi <glenn.latomme@gmail.com>
2024-08-07 15:14:50 +0200

    ci: merge lcov into one and use that

--- apps/worker/sync/jest.config.ts
+++ apps/worker/sync/jest.config.ts
@@ -7,5 +7,5 @@ export default {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/worker/sync',
+  coverageDirectory: '../../../coverage/apps/worker/sync',
 };
