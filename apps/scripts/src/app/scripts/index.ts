d58ac5d8c2f4becfb08d94179a8d14db382d9309
Glenn Latomme <glenn.latomme@arcelormittal.com>
2024-08-20 16:09:36 +0200

    feat: exporting players with their calculated downgrade points

--- apps/scripts/src/app/scripts/index.ts
+++ apps/scripts/src/app/scripts/index.ts
@@ -1,2 +1,4 @@
-// start:ng42.barrel
-// end:ng42.barrel
+export * from './add-locations-to-cp';
+export * from './changed-encounters';
+export * from './export-players-with-ranking';
+export * from './incorrect-change-encounters';
