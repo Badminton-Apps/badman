8b22b5d34e2545747a2a407354805a39cbf46777
cskiwi <glenn.latomme@gmail.com>
2024-09-05 12:18:50 +0200

    fix: further improvements on export of players ranking

--- apps/scripts/src/app/app.module.ts
+++ apps/scripts/src/app/app.module.ts
@@ -3,10 +3,10 @@ import { VisualModule } from '@badman/backend-visual';
 import { configSchema, load } from '@badman/utils';
 import { Logger, Module, OnModuleInit } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { PlayersWrongRankingRunner } from './scripts';
+import { ExportPlayersWithRanking } from './scripts';
 
 @Module({
-  providers: [PlayersWrongRankingRunner],
+  providers: [ExportPlayersWithRanking],
   imports: [
     ConfigModule.forRoot({
       cache: true,
@@ -20,7 +20,7 @@ import { PlayersWrongRankingRunner } from './scripts';
 export class ScriptModule implements OnModuleInit {
   private readonly logger = new Logger(ScriptModule.name);
 
-  constructor(private fixer: PlayersWrongRankingRunner) {}
+  constructor(private fixer: ExportPlayersWithRanking) {}
 
   async onModuleInit() {
     this.logger.log('Running script');
