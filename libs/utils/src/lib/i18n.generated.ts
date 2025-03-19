b632bee6cd5ba8b8461eaae5e780f17ca6dfed78
nikkizol <nikita.zholkovskyi@gmail.com>
2025-03-19 17:30:50 +0100

    fix: change translations for club edit location

--- libs/utils/src/lib/i18n.generated.ts
+++ libs/utils/src/lib/i18n.generated.ts
@@ -2261,8 +2261,8 @@ export type I18nTranslations = {
                         "form": {
                             "name": string;
                             "street": string;
-                            "number": string;
-                            "zip": string;
+                            "streetNumber": string;
+                            "postalcode": string;
                             "city": string;
                             "state": string;
                             "phone": string;
