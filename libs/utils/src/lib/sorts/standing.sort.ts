086c28ff632350197e98a9a97c129a5d9a876d91
cskiwi <glenn.latomme@gmail.com>
2025-04-13 16:08:23 +0200

    fix: refactor standing sorting logic to use dedicated sortStanding utility

--- libs/utils/src/lib/sorts/standing.sort.ts
+++ libs/utils/src/lib/sorts/standing.sort.ts
@@ -1,64 +1,7 @@
 export const sortStanding = (a: Partial<SortStandingType>, b: Partial<SortStandingType>) => {
-  const teamA = {
-    points: a?.points || 0,
-    won: a?.won || 0,
-    tied: a?.tied || 0,
-    lost: a?.lost || 0,
-    gamesWon: a?.gamesWon || 0,
-    gamesLost: a?.gamesLost || 0,
-    setsWon: a?.setsWon || 0,
-    setsLost: a?.setsLost || 0,
-    totalPointsWon: a?.totalPointsWon || 0,
-    totalPointsLost: a?.totalPointsLost || 0,
-  };
-
-  const teamB = {
-    points: b?.points || 0,
-    won: b?.won || 0,
-    tied: b?.tied || 0,
-    lost: b?.lost || 0,
-    gamesWon: b?.gamesWon || 0,
-    gamesLost: b?.gamesLost || 0,
-    setsWon: b?.setsWon || 0,
-    setsLost: b?.setsLost || 0,
-    totalPointsWon: b?.totalPointsWon || 0,
-    totalPointsLost: b?.totalPointsLost || 0,
-  };
-
-  if (teamA.points > teamB.points) {
-    return -1;
-  } else if (teamA.points < teamB.points) {
-    return 1;
-  }
-
-  if (teamA.won - teamA.lost > teamB.won - teamB.lost) {
-    return -1;
-  } else if (teamA.won - teamA.lost < teamB.won - teamB.lost) {
-    return 1;
-  }
-
-  if (teamA.tied > teamB.tied) {
-    return -1;
-  } else if (teamA.tied < teamB.tied) {
-    return 1;
-  }
-
-  if (teamA.gamesWon - teamA.gamesLost > teamB.gamesWon - teamB.gamesLost) {
-    return -1;
-  } else if (teamA.gamesWon - teamA.gamesLost < teamB.gamesWon - teamB.gamesLost) {
-    return 1;
-  }
-
-  if (teamA.totalPointsWon - teamA.totalPointsLost > teamB.totalPointsWon - teamB.totalPointsLost) {
-    return -1;
-  } else if (
-    teamA.totalPointsWon - teamA.totalPointsLost <
-    teamB.totalPointsWon - teamB.totalPointsLost
-  ) {
-    return 1;
-  }
-
-  return 0;
+  const aScore = (a.won ?? 0) - (a.lost ?? 0) - (a.tied ?? 0);
+  const bScore = (b.won ?? 0) - (b.lost ?? 0) - (b.tied ?? 0);
+  return bScore - aScore;
 };
 
 type SortStandingType = {
