89cc1ea381c322a67f90e7f2810e29000d1912db
cskiwi <glenn.latomme@gmail.com>
2025-07-27 12:40:32 +0200

    feat: refactor build configurations and update GraphQL module integration

--- apps/worker/belgium/flanders/places/webpack.config.js
+++ apps/worker/belgium/flanders/places/webpack.config.js
@@ -2,19 +2,10 @@ const { NxWebpackPlugin } = require('@nx/webpack');
 const { join } = require('path');
 
 module.exports = {
-  output: {
-    path: join(__dirname, '../../../../../dist/apps/worker/belgium/flanders/places'),
-  },
   plugins: [
     new NxWebpackPlugin({
-      target: 'node',
-      compiler: 'tsc',
-      sourceMap: true,
-      main: './src/main.ts',
-      tsConfig: './tsconfig.app.json',
-      assets: ['./src/assets'],
-      optimization: false,
       outputHashing: 'none',
+      optimization: false,
     }),
   ],
 };
